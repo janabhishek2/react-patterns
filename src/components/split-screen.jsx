481b6c4cf9ed75bc09dc836600b4c3b9b5b0a556
Abhishek Jan <janabhishek2@gmail.com>
2024-04-07 00:06:54 +0530

    leftWidth and rightWidth props added for split-screen pattern

--- src/components/split-screen.jsx
+++ src/components/split-screen.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { styled } from "styled-components";
 
-export default function ({ Left, Right }) {
+export default function ({ Left, Right, leftWidth = 1 , rightWidth = 1 }) {
     const Container = styled.div`
         display: flex;
     `;
 
     const Panel = styled.div`
-        flex: 1;
+        flex: ${(props) => props.flex};
+        background-color: ${(props) => props.bgColor}
     `;
+
     return (
         <Container>
-            <Panel>
+            <Panel flex={leftWidth} bgColor="red">
                 <Left />
             </Panel>
-            <Panel>
+            <Panel flex={rightWidth} bgColor="green">
                 <Right />
             </Panel>
         </Container>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import SplitScreen from "./components/split-screen.jsx";
-
+import './index.css';
 
 const Left = () => <h1>Left</h1>;
 const Right = () => <h1>Right</h1>;
 
 
 function App() {
-    return <SplitScreen Left={Left} Right={Right}/>
+    return <SplitScreen Left={Left} Right={Right} leftWidth={1} rightWidth={3} />
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
