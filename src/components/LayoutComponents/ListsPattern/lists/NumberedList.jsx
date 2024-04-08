0daaf804ee2ca047938cf0502728abba37632fe4
Abhishek Jan <janabhishek2@gmail.com>
2024-04-08 14:10:59 +0530

    Restructuring for readability

--- src/components/LayoutComponents/ListsPattern/lists/NumberedList.jsx
+++ src/components/LayoutComponents/ListsPattern/lists/NumberedList.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { styled } from 'styled-components';
 
+const ListWrapper = styled.ul`
+text-decoration: none;
+list-style: none;
+`;
+
+const ListItemComponentWrapper = styled.div`
+display: flex;
+flex-direction: row;
+`;
+
+const IndexWrapper = styled.div``;
+
+const ContentWrapper = styled.div`
+margin-left: 1rem;
+`;
+
 function NumberedList({ source, data, ItemComponent }) {
-
-  const ListWrapper = styled.ul`
-    text-decoration: none;
-    list-style: none;
-  `;
-
-  const ListItemComponentWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-  `;
-
-  const IndexWrapper = styled.div``;
-
-  const ContentWrapper = styled.div`
-    margin-left: 1rem;
-  `;
 
   return (
     <ListWrapper>
         {
             data.map((item, index) => {
                 return (
-                   <ListItemComponentWrapper>
+                   <ListItemComponentWrapper key={index}>
                         <IndexWrapper>{index + 1}.</IndexWrapper>
                         <ContentWrapper>
                             <li key={index}>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import './index.css';
-import Lists from "./components/ListsPattern/Lists.jsx";
+import Lists from "./components/LayoutComponents/ListsPattern/Lists.jsx";
 
 function App() {
     return (<Lists />);
