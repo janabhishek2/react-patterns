c3ebf2ee3e14151bbd3343a24a63a1ed0363cee4
Abhishek Jan <janabhishek2@gmail.com>
2024-06-21 12:11:29 +0530

    useLayout effect

--- src/components/AdvancedReact/LayoutEffect/LayoutEffect.jsx
+++ src/components/AdvancedReact/LayoutEffect/LayoutEffect.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 function LayoutEffect() {
-    return (
-        <div>LayoutEffect</div>
-    )
+    const [top, setTop] = useState(0);
+    const [show, setShow] = useState(false);
+
+    const buttonRef = useRef(null);
+
+    console.log(top);
+    const handleClick = () => {
+        setShow(!show);
+    };
+    
+    const now = performance.now();
+
+    while(now > performance.now() - 300) {
+        null;
+    }
+    useLayoutEffect(() => {
+        if(!buttonRef.current || !show) setTop(0);
+        else {
+            const { bottom } = buttonRef.current.getBoundingClientRect();
+            setTop(bottom + 30);
+        }
+    }, [show]);
+
+    return (<>
+        <button ref={buttonRef} onClick={handleClick}>Click</button>
+        {show && <div style={{
+            border: "1px solid black",
+            position: "fixed",
+            padding: '20px',
+            top: top
+        }}>
+            tooltip data
+        </div>}
+    </>)
 }
 
 export default LayoutEffect;
