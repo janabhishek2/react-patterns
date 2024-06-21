04d8bf5954b50e7cbe99aed6ae97c2b4031fc5c8
Abhishek Jan <janabhishek2@gmail.com>
2024-06-21 08:26:47 +0530

    Layout effect setup

--- src/components/AdvancedReact/Advanced.jsx
+++ src/components/AdvancedReact/Advanced.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ErrorBoundaries from './ErrorBoundaries';
 import ForwardRef from './ForwardRefs';
 import Keys from './Keys';
+import LayoutEffect from './LayoutEffect';
 import Portals from './Portals';
 
 function Advanced() {
   return (
-    <Portals />
+    <LayoutEffect />
   )
 }
 
---
+++ src/components/AdvancedReact/LayoutEffect/LayoutEffect.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LayoutEffect() {
+    return (
+        <div>LayoutEffect</div>
+    )
+}
+
+export default LayoutEffect;
