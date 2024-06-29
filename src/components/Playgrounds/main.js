41a239b67e0650092d2bf85b1ff6b24f22e334c4
Abhishek Jan <janabhishek2@gmail.com>
2024-06-29 16:52:42 +0530

    This: inside a method of object

--- src/components/Playgrounds/main.js
+++ src/components/Playgrounds/main.js
@@ -1,56 +1,10 @@
-class UserProfile {
-    setUserName(username) {
-        if(typeof username === "string" && username.length > 0) {
-            this._username = username;
-        } else {
-            throw new Error("Invalid username.");
-        }
+const person = {
+    name: "Conan",
+    city: "LA",
+    sing: function() {
+        // this refers to object
+        console.log("LALALA", this);
     }
+};
 
-    setEmail(email) {
-        if(email.includes("@")) {
-            this._email = email;
-        } else {
-            throw new Error("Invalid email.");
-        }
-    }
-
-    setBirthDate(birthDate) {
-        const ms = Date.parse(birthDate);
-        if(!isNaN(ms)) {
-            this._birthdate = birthDate;
-        } else {
-            throw new Error("Invalid birthdate.")
-        }
-    }
-
-    constructor(username, email, birthdate) {
-        this.setUserName(username);
-        this.setEmail(email);
-        this.setBirthDate(birthdate);
-    }
-
-    get username() {
-        return this._username;
-    }
-
-    get email() {
-        return this._email;
-    }
-
-    get birthdate() {
-        return this._birthdate;
-    }
-
-    set username(username) {
-        this.setUserName(username);
-    }
-
-    set email(email) {
-        this.setEmail(email);
-    }
-
-    set birthdate(birthDate) {
-        this.setBirthDate(birthDate);
-    }
-}
+person.sing();
