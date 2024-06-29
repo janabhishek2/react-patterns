1126e03f716ca9196c2a3c41471c1471c271f486
Abhishek Jan <janabhishek2@gmail.com>
2024-06-29 09:53:50 +0530

    private methods

--- src/components/Playgrounds/main.js
+++ src/components/Playgrounds/main.js
@@ -1,6 +1,16 @@
 class Circle {
     // private class fields begin with #
+
+    // class field means declaring all variables that will be used in the class before-hand.
+    // for private fields and public fields, we declare class fields.
+    // for private methods we do not need class fields.
+
     #radius = 3;
+
+    // declare a private method starting with pound sign. (#)
+    #getDiameter() {
+        return this.#radius * 2;
+    }
 
     constructor(radius) {
         this.#radius = radius;
@@ -10,7 +20,12 @@ class Circle {
         return this.#radius;
     }
 
+    get diameter() {
+        // using private method inside a function of class.
+        return this.#getDiameter();
+    }
+
 }
 
 const circle = new Circle(3);
-console.log(circle.radius);
+console.log(circle.diameter);
