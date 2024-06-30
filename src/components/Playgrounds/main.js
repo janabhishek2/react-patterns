1382304f23a48e65b547d844d7c388ea13bb8622
Abhishek Jan <janabhishek2@gmail.com>
2024-06-30 12:08:18 +0530

    Using bind to bind the this object to callback of setInterval

--- src/components/Playgrounds/main.js
+++ src/components/Playgrounds/main.js
@@ -1,10 +1,17 @@
-const btn = document.querySelector("#clickMe");
+class Counter {
+    constructor(startingNum = 0, increment = 1) {
+        this.count = startingNum;
+        this.increment = increment;
+    }
 
-const conan = {
-    name: "Conan",
-    sings: function() {
-        console.log(`${this.name} sings LALAL`);
+    start() {
+        // we need to bind setInterval to Counter instance; by default the callback is bound to window.
+        setInterval(function() {
+            console.log("Value is: ", this.count);
+            this.count += this.increment;
+        }.bind(this), 1000)
     }
 };
 
-btn.addEventListener("click", conan.sings.bind(conan));
+const counter = new Counter();
+counter.start();
