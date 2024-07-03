48037f7a2ce2b9841b0afccae5caeeed29633c1e
Abhishek Jan <janabhishek2@gmail.com>
2024-07-03 17:38:54 +0530

    using classList to set classNames

--- src/components/DOM/script.js
+++ src/components/DOM/script.js
@@ -2,10 +2,6 @@ const button = document.querySelector("button");
 const section = document.querySelector("section");
 
 button.addEventListener("click", () => {
-    const sectionClasses = section.className;
-    if(sectionClasses.includes("invisible")) {
-        section.className = 'red-bg visible';
-    } else {
-        section.className = 'red-bg invisible';
-    }
+   section.classList.toggle("visible");
+   section.classList.toggle("invisible");
 })
