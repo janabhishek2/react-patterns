067b98f08290c9ef698ef1a299ef28cc0a886179
Abhishek Jan <janabhishek2@gmail.com>
2024-07-13 12:43:05 +0530

    insert after begin

--- src/components/DOM/script.js
+++ src/components/DOM/script.js
@@ -1,7 +1,16 @@
-const button = document.querySelector("button");
-const section = document.querySelector("section");
-
 const div = document.querySelector("div");
 
 // all children of div are re-rendered.
-div.innerHTML += "<p>Hello world</p>";
+let adjacentHtmlAfterbegin = "<p>Hello I am appended after begin</p>";
+let adjacentHtmlBeforeEnd = "<p>Hello, I am appended before end</p>";
+
+div.style.border = "1px solid black";
+div.style.padding = "10px";
+
+const button = document.querySelector("button");
+button.textContent = "Insert after begin";
+
+button.addEventListener("click", function(e) {
+   console.log(e);
+   div.insertAdjacentHTML("afterbegin", adjacentHtmlAfterbegin);
+});
