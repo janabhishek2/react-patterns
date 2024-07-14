df41b06586fb5e6437e82ac9616e70f04d09b4fc
Abhishek Jan <janabhishek2@gmail.com>
2024-07-14 06:48:46 +0530

    appendCHild: append element node at end of list

--- src/components/DOM/script.js
+++ src/components/DOM/script.js
@@ -1,17 +1,7 @@
-const div = document.querySelector("div");
+const list = document.querySelector("ul");
 
-// all children of div are re-rendered.
-let adjacentHtmlAfterbegin = "<p>Hello I am appended after begin</p>";
-let adjacentHtmlBeforeEnd = "<p>Hello, I am appended before end</p>";
+const newLi = document.createElement("li");
 
-div.style.border = "1px solid black";
-div.style.padding = "10px";
+newLi.textContent = "List item -- new";
 
-const button = document.querySelector("button");
-button.textContent = "Insert before end..";
-
-button.addEventListener("click", function(e) {
-   console.log(e);
-   // benefit: does not re-render children like innerHTML ONLY ADDS ELEMENT BEFORE END;
-   div.insertAdjacentHTML("beforeend", adjacentHtmlBeforeEnd);
-});
+list.appendChild(newLi);
