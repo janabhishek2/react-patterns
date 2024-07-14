3ebc8132d8559dfa5146430c4ab55fbc6e6b810f
Abhishek Jan <janabhishek2@gmail.com>
2024-07-14 12:40:48 +0530

    using cloneNode to shallow/deep clone element nodeS

--- src/components/DOM/script.js
+++ src/components/DOM/script.js
@@ -1,9 +1,17 @@
 const list = document.querySelector("ul");
 
-const secondLi = list.children[1];
+const newLi = document.createElement("li");
+newLi.textContent = "New Li";
 
-const newElement = document.createElement("li");
-newElement.textContent = "newly created item";
+// replace firstChild Element with newLi
 
-// Use this method in place of after: for better browser support
-secondLi.insertAdjacentElement("afterend", newElement);
+const firstChild = list.firstElementChild;
+
+// deletes the first elementChild and inserts newLi in its place.
+firstChild.replaceWith(newLi);
+
+// true is passed to deep clone the children of the parent node; default is false to make shallow copy
+const newLi2 = newLi.cloneNode(true);
+
+// moves the newLi to last and appends newLi2 to it.
+list.append(newLi, newLi2);
