552f6576dc1bd088180927c23220668fc734a1b1
Abhishek Jan <janabhishek2@gmail.com>
2024-07-18 07:42:49 +0530

    removeChild

--- src/components/DOM/script.js
+++ src/components/DOM/script.js
@@ -1,12 +1,5 @@
 const list = document.querySelector("ul");
 
-const listItems = list.children; // htmlCollection --> dynamic (old)
-const listItems1 = list.querySelectorAll("li"); // nodelist --> static (new method)
+list.parentNode.removeChild(list);
 
-const newLi = document.createElement("li");
-newLi.textContent = "New lI";
-
-list.append(newLi);
-
-// all getXByY methods return live node-lists : htmlCollection
-// querySelectorAll returns static node lists -> NodeList[];
+// 2 methods: .remove() and .removeChild()
