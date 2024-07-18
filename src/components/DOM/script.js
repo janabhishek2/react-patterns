511a94dc7219af168f9cf2b2584bde484eacb665
Abhishek Jan <janabhishek2@gmail.com>
2024-07-18 06:53:19 +0530

    HtmlCollection vs NodeList

--- src/components/DOM/script.js
+++ src/components/DOM/script.js
@@ -1,17 +1,12 @@
 const list = document.querySelector("ul");
 
+const listItems = list.children; // htmlCollection --> dynamic (old)
+const listItems1 = list.querySelectorAll("li"); // nodelist --> static (new method)
+
 const newLi = document.createElement("li");
-newLi.textContent = "New Li";
+newLi.textContent = "New lI";
 
-// replace firstChild Element with newLi
+list.append(newLi);
 
-const firstChild = list.firstElementChild;
-
-// deletes the first elementChild and inserts newLi in its place.
-firstChild.replaceWith(newLi);
-
-// true is passed to deep clone the children of the parent node; default is false to make shallow copy
-const newLi2 = newLi.cloneNode(true);
-
-// moves the newLi to last and appends newLi2 to it.
-list.append(newLi, newLi2);
+// all getXByY methods return live node-lists : htmlCollection
+// querySelectorAll returns static node lists -> NodeList[];
