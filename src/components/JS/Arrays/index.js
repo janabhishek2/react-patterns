2dde6c3b424ba777f9d3614b69f58923a5d557d4
Abhishek Jan <janabhishek2@gmail.com>
2024-07-23 09:47:45 +0530

    different types that can be used in arrays

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,7 +1,4 @@
-// here prototype would be NodeList
-const listItems = document.querySelectorAll("li");
+//  We can have similar/differnt type of data in arrays
 
-// here prototype will be array
-const arrayListItems = Array.from(listItems);
-
-console.log(arrayListItems);
+const arr = [1, 2, 3, "hello", [1, 2], [[1, 2], [3, 4]]];
+console.log(arr);
