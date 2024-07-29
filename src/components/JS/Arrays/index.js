a9e1aa875c11c8171de6ae0f4d5f60638070ea60
Abhishek Jan <janabhishek2@gmail.com>
2024-07-29 12:06:41 +0530

    this keyword and new

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,6 +1,14 @@
-function test() {
+function Dog(name, breed) {
+    this._name = name;
+    this._breed = breed;
+
     console.log(this);
 }
 
-// window object
-test();
+// this refers to window
+// window._name = "abcd";
+const p = Dog("abcd", "Efg");
+
+// this creates a new object with keys _name and _breed
+const dog = new Dog("Test", "Breed");
+
