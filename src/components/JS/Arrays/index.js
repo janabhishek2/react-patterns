c21c6980e97c0212d841db1ef99f28cdb8287b31
Abhishek Jan <janabhishek2@gmail.com>
2024-07-29 12:13:40 +0530

    Notes: this and new

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -10,5 +10,8 @@ function Dog(name, breed) {
 const p = Dog("abcd", "Efg");
 
 // this creates a new object with keys _name and _breed
+// this is called a factory function / constructor function
+// cf are used in conjunction with new keyword to create objects.
 const dog = new Dog("Test", "Breed");
 
+// new keyword: creates an empty object ; binds the this keyword to that object and returns it
