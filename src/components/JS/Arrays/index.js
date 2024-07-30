d7cab645d899a420b4ec44c93b8c89b29cb28179
Abhishek Jan <janabhishek2@gmail.com>
2024-07-30 09:14:26 +0530

    comments: functions for classes/objects

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -8,6 +8,8 @@ class Dog {
         console.log("This is a test fn");
     }
     // here bark is bound to prototype of object created by new keyword.
+
+    // dog1.func === dog2.func is true as same method is used for all objects
     bark() {
         console.log(`${this.name} says woof !`);
     }
@@ -23,6 +25,7 @@ function Doggy(name, breed) {
     // here bark is bound to object created from constructor function
 
     // because every object this contains its own copy of method sleep/bark.
+    // dogg1.func === doggy2.func is false
     this.bark = function() {
         console.log(`${this._name} says woof !`);
     }
