5fa90a475d206efdcb1c92fa0a68b9496f22ebd4
Abhishek Jan <janabhishek2@gmail.com>
2024-07-30 19:20:57 +0530

    constructor on functions prototype

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -7,5 +7,14 @@ Dog.prototype.bark = function() {
     console.log(`${this._name} says woof!`);
 };
 
-const dog = new Dog("Test name", "Test Breed");
+// every function has prototype method
+// Dog.prototype.constructor refers to the same dog function.
+
+const dog = new Dog.prototype.constructor("Yet", "n");
+
+// this can also be accessed via __proto__ on dog.
+
+const newDog = new dog.__proto__.constructor("Test", "3");
+
+
 
