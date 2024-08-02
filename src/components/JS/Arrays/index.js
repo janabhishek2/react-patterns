82bb132b5909e2afdc80d92aad20a3c48d60f1bd
Abhishek Jan <janabhishek2@gmail.com>
2024-08-02 09:45:57 +0530

    nullish coalescing assignment used in functions

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -2,11 +2,25 @@
 
 // logical: && , || , ??
 
+// nullish coalescing assignment
+
 const person = {
-    name: "Abhishek"
+    name: null
 };
 
-// if person name exists(truthy) then set it it rocky
-person.name &&= "Rocky";
+person.name ??= "Abhishek";
+// person.name ?? (person.name = "Abhishek")
 
 console.log(person);
+
+function doSetup(object = {}) {
+    object.timeout ??= 3000;
+    object.retries ??= 5;
+
+    return object;
+}
+
+const ans = doSetup();
+console.log(ans); 
+
+// sets timeout and retries to 3000 and 5.
