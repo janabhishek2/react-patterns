b2019c24b652252a0ff0c597a0fdbf6a36d5fb45
Abhishek Jan <janabhishek2@gmail.com>
2024-08-03 18:25:53 +0530

    comments: hoisting

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -3,5 +3,6 @@
 // var keyword defs are moved to the top of scope chain: function/global scope only and set to undefined.
 // they are assigned values when they are interpreted by code.
 
+// we don't get the undefined error while using var keyword; this is not true with let/const.
 console.log(food); // returns undefined
 var food = "pizza";
