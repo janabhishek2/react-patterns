927f43f5b1328ca0032582f78c9615582b68bf45
Abhishek Jan <janabhishek2@gmail.com>
2024-08-03 17:44:14 +0530

    scope chain for variable age

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -2,17 +2,26 @@
 
 // 1. Look in local scope
 // 2. Look in ext functions
-// 3. Look in global scope
+// 3. Look in global scope --> can be accessed from console directly
 
 let age = 10;
 
 function outer() {
     let age = "ageless";
     function inner() {
-        let age = "eternal";
-        console.log(age);
+        // age = "internal age";
+        function superInner() {
+            console.log("Super inner age is: ", age);
+        }
+        superInner();
+        console.log("Inner age is: ", age);
     }
     inner();
+    console.log("Outer age is: ", age);
 }
+
+outer();
+console.log("global age", age);
+
 
 // if age is not found in local scope; js will look for outer functions else global scope for age variable.
