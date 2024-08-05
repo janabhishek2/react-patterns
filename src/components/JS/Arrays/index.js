0774d7abe6999f2adcfe1742717b55ae15699976
Abhishek Jan <janabhishek2@gmail.com>
2024-08-05 18:25:56 +0530

    Exercise2: create account

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,24 +1,45 @@
-function guessingGame() {
-    let fixNum = Math.ceil(Math.random() * 100);
-    let won = false;
-    let guesses = 0;
-    return function game(input) {
-        if(won) {
-            return `The game is over, you already won!`;
-        }
-        ++guesses;
-        if(input < fixNum) {
-            return `${input} is too low!`;
-        }
-        else if(input > fixNum) {
-            return (`${input} is too high!`);
-        }
-        else{
-            won = true;
-            const guess = guesses === 1 ? "guess" : "guesses";
-            return `You win! You found ${fixNum} in ${guesses} ${guess}.`
+function createAccount(initPin, initialAmount) {
+    let currentBalance = initialAmount;
+    let currPin = initPin;
+
+    function checkBalance(pin) {
+        if(currPin !== pin) {
+            return "Invalid PIN.";
+        } else {
+            return currentBalance;
         }
     }
-}
 
-let game = guessingGame();
+    function deposit(pin, amt) {
+        if(currPin !== pin) {
+            return "Invalid PIN.";
+        } else {
+            currentBalance += amt;
+            return `Successfully deposited ${amt}. Current balance: ${currentBalance}.`
+        }
+    }
+
+    function withdraw(pin, amt) {
+        if(currPin !== pin) {
+            return "Invalid PIN.";
+        } else if(currentBalance < amt) {
+            return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+        } else {
+            currentBalance -= amt;
+            return `Successfully withdrew ${amt}. Current balance: ${currentBalance}.`
+        }
+    }
+
+    function changePin(oldPin, newPin) {
+        if(oldPin !== currPin) {
+            return "Invalid PIN.";
+        }  else {
+            currPin = newPin;
+            return "PIN successfully changed!"
+        }
+    }
+    
+    return {
+        checkBalance, deposit, withdraw, changePin
+    }
+}
