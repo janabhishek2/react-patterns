96f04dc6daf6352a7d6516e53d17a2563929ded3
Abhishek Jan <janabhishek2@gmail.com>
2024-08-07 07:57:16 +0530

    alert message displayed

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,1 +1,20 @@
 // Timers...
+
+function showNotification(message, duration) {
+    const alertMessage = document.createElement("div");
+    alertMessage.style.margin = "auto";
+    alertMessage.style.width = "60%";
+    alertMessage.style.padding = "12px";
+    alertMessage.textContent = message;
+    document.body.insertAdjacentElement("beforebegin", alertMessage);
+
+    function removeNotification() {
+        alertMessage.remove();
+    };
+
+    setTimeout(() => {
+        removeNotification();
+    }, duration);
+}
+
+showNotification("Hello there !! ", 3000);
