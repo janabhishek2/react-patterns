d6e1113ef12d37ab9291cdacec59cdfb31bdc53d
Abhishek Jan <janabhishek2@gmail.com>
2024-08-07 13:16:04 +0530

    Live search on input

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,17 +1,10 @@
 // Timers...
 
-const button = document.querySelector("button");
+const queryApi = () => {
+    console.log("Making an api request...");
+};
 
-const timeoutId = setTimeout(() => {
-    window.location.href = "https://www.google.com";
-}, 5000);
+const searchInput = document.querySelector("#search");
 
-function preventRedirect() {
-    console.log("Redirection cancelled !!");
-    clearTimeout(timeoutId);
-}
-
-button.addEventListener("click", preventRedirect);
-
-// original link
-// http://127.0.0.1:5500/src/components/JS/Arrays/index.html
+// live search
+searchInput.addEventListener("input", queryApi);
