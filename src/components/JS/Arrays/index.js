2e396abc291000550dbed5f9388b26ff2de358ee
Abhishek Jan <janabhishek2@gmail.com>
2024-08-07 10:22:00 +0530

    Using intervalId to clear Interval '

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,13 +1,17 @@
 // Timers...
 
 const h1 = document.getElementById("timer");
-let secondsRemaining = 10;
+let secondsRemaining = 3;
 h1.textContent = secondsRemaining;
 
-setInterval(function() {
-    if(secondsRemaining) {
+const intervalId = setInterval(function() {
+    if(secondsRemaining > 0) {
+        h1.textContent = secondsRemaining;
         secondsRemaining -= 1;
+    } else {
+        h1.textContent = "Time is UPP!";
+        // inner function gets access to outer variable intervalid via closure
+        clearInterval(intervalId);
     }
-    h1.textContent = secondsRemaining;
 }, 1000);
 
