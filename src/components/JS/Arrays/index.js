099abee302d5f207adb9f63dc60bf2b1e43c81ac
Abhishek Jan <janabhishek2@gmail.com>
2024-08-07 13:32:00 +0530

    Debounced search

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,10 +1,18 @@
 // Timers...
 
-const queryApi = () => {
-    console.log("Making an api request...");
+const queryApi = (value) => {
+    console.log("Making an api request...", value);
 };
 
 const searchInput = document.querySelector("#search");
 
-// live search
-searchInput.addEventListener("input", queryApi);
+// debounced search
+
+let timeoutId = null;
+
+searchInput.addEventListener("input", (e) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+        queryApi(e.target.value);
+    }, 1000);
+});
