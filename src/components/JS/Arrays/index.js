f72359fcea22196525698d44ea9d2eb665dd2b07
Abhishek Jan <janabhishek2@gmail.com>
2024-08-11 14:37:17 +0530

    FP basicS

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,1 +1,16 @@
 // Functional Programming
+
+// TO sum 2 numbers there are 2 approaches
+// Imperative
+
+// using a state to manipulate
+let sum = 0;
+for(let i=1; i<=5; i++) {
+    sum += i;
+}
+
+// FP approach
+
+// we are using pure functions to manipulate data.
+const ans = [1, 2, 3, 4, 5].reduce((acc, curr) => acc + curr, 0);
+console.log(ans);
