0f2b1803fe74a0574ba36bf2b7c066cc2134e29e
Abhishek Jan <janabhishek2@gmail.com>
2024-08-11 16:35:44 +0530

    Mutating object

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,24 +1,10 @@
-// HOF
-// Function recives another function as an arg, returns a function or does both..
+// immutablity
 
-function doTwice(func) {
-    func();
-    func();
-}
+const object = {
+    name: 'ABhishek',
+    age: 25
+};
 
-doTwice(() => console.log("Hiii"));
+object.name = "Test";
 
-// factory functions 
-
-// HOF: returns a function 
-function multiplyBy(factor) {
-    return function(number) {
-        return number * factor;
-    }
-}
-
-const double = multiplyBy(2);
-const triple = multiplyBy(3);
-
-console.log(double(3));
-console.log(triple(5));
+console.log(object);
