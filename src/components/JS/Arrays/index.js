06cef36087a0aa570da70d239e4377a123f6524c
Abhishek Jan <janabhishek2@gmail.com>
2024-08-11 16:10:39 +0530

    Impure function: 2

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -3,7 +3,18 @@
 let value = 2;
 
 // Impure: because value is mutated here.
+// no mutation, no side effects for pure functions...
+// same input: same output
 function squareAndUpdateValue(num) {
     value = num * num;
     return value;
 }
+
+// Impure: because it updates nums array as well.
+function addToArray(arr, val) {
+    arr.push(val);
+}
+
+const nums = [1, 2, 3];
+addToArray(nums, 4);
+console.log(nums);
