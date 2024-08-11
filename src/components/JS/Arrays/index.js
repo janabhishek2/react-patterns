f4c1e248c7b36858e97572369fe627ddbe989532
Abhishek Jan <janabhishek2@gmail.com>
2024-08-11 14:50:13 +0530

    functions added as values to array

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,22 +1,13 @@
-// Functional Programming
+// JS functions are first class objects/citizens : functions can be treated the same way as any other variable.
+// we can assign functions to vars, pass functions to functions/return from functions etc.
 
-// Get even nums
-
-const nums = [1, 2, 3, 4, 5];
-
-// imperative
-let evens = [];
-for(let num of nums) {
-    if(num%2 === 0) {
-        evens.push(num);
+const funcs = [
+    function(name) {
+        console.log(`I love you.. ${name}`);
+    },
+    function(name) {
+        console.log(`I hate you..., ${name}`)
     }
-}
+];
 
-// functional
-
-const ans = nums.filter((item) => item%2 === 0);
-
-console.log(ans);
-
-// Imperative: logic is more... 
-// Functional: there are built in useful methods to achieve sopmething.
+funcs[1]("Shweta");
