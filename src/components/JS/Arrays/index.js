028e4e177b3c63669bdfa7ec0e0ac15a6c68833d
Abhishek Jan <janabhishek2@gmail.com>
2024-08-13 12:17:25 +0530

    Composition: basics

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,29 +1,8 @@
-// Writing partial functions
+// Composition: passing return values of 1 funciton to other funciton
 
-const partial = (func, ...fixed) => {
-    return function(...rem) {
-        return func(...fixed, ...rem);
-    }
-};
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
 
-const fetchData = (url, apiKey, payload) => {
-    const urlSearchParams = new URLSearchParams(payload);
-    
-    const fullUrl = `${url}?${urlSearchParams.toString()}`;
-    console.log('Making api call', fullUrl);
-    console.log('Api key', apiKey);
-};
+const ans = add(2, multiply(10,3));
 
-const myApiKey = "abcd";
-const myApiUrl = "https://contacts-api.51b.dev";
-
-const googleApiKey = "google-api-key";
-const googleUrl = "https://google.51b.dev";
-
-const fetchMyApi = partial(fetchData, myApiUrl, myApiKey);
-const fetchGoogleApi = partial(fetchData, googleUrl, googleApiKey);
-
-fetchMyApi({
-    param1: 'value1',
-    param2: "value2"
-});
+console.log(ans);
