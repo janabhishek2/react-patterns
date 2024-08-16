7854495b4458edb308172f6be2200f4958141545
Abhishek Jan <janabhishek2@gmail.com>
2024-08-16 17:12:11 +0530

    Dice game using class components

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,16 +1,40 @@
-function add(a, b, c) {
-    return a + b + c;
-};
+class DiceGame {
+    infoSection = null;
+    displayButton = null;
 
-function addCurry(a) {
-    return function(b) {
-        return function(c) {
-            // variables a and b accesible via closure
-            return a+b+c;
+    static tries = 0;
+    constructor(buttonId, textId) {
+
+        this.infoSection = document.getElementById(textId);
+        this.infoSection.textContent = "Click the above button to start the game!";
+
+        this.displayButton = document.getElementById(buttonId);
+        this.displayButton.addEventListener("click", this.rollDice);
+    }
+
+    getRandomNumber() {
+        return Math.ceil(Math.random() * 6);
+    }
+
+    isGameWon(selected) {
+        return selected === 6;
+    }
+
+    rollDice = () => {
+        const rand = this.getRandomNumber();
+        ++DiceGame.tries;
+        if(this.isGameWon(rand)) {
+            this.infoSection.textContent = `You have won! after ${DiceGame.tries} tries`;
+            DiceGame.tries = 0;
+            this.displayButton.textContent = "Click Me!";
+        } else {
+            this.infoSection.textContent = `Number ${rand} popped up. Press the button to try again!`;
+            this.displayButton.textContent = "Try Again ?";
         }
     }
-};
 
-const curry = addCurry(3)(4)(5);
+}
 
-console.log(curry);
+const s = new DiceGame("click_me", "text");
+
+console.log(s);
