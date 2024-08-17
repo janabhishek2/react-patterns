8ae16568f7de510e7492c78e048f5515216341d6
Abhishek Jan <janabhishek2@gmail.com>
2024-08-17 11:13:35 +0530

    Dice game using procedures

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,40 +1,31 @@
-class DiceGame {
-    infoSection = null;
-    displayButton = null;
+const getRandomRoll = () => Math.ceil(Math.random() * 6);
 
-    static tries = 0;
-    constructor(buttonId, textId) {
+// writing pure functions
+const isWon = (num) => num === 6;
 
-        this.infoSection = document.getElementById(textId);
-        this.infoSection.textContent = "Click the above button to start the game!";
-
-        this.displayButton = document.getElementById(buttonId);
-        this.displayButton.addEventListener("click", this.rollDice.bind(this));
-    }
-
-    getRandomNumber() {
-        return Math.ceil(Math.random() * 6);
-    }
-
-    isGameWon(selected) {
-        return selected === 6;
-    }
-
-    rollDice() {
-        const rand = this.getRandomNumber();
-        ++DiceGame.tries;
-        if(this.isGameWon(rand)) {
-            this.infoSection.textContent = `You have won! after ${DiceGame.tries} tries`;
-            DiceGame.tries = 0;
-            this.displayButton.textContent = "Click Me!";
+const handleButtonClick = function(tries, textElement) {
+    return function() {
+        const numberGenerated = getRandomRoll();
+        ++tries;
+        if(isWon(numberGenerated)) {
+            textElement.textContent = `You won after ${tries} tries!`;
+            tries = 0;
+            // this gets access to buttonElement that triggered this event
+            this.textContent = "Click me!"
         } else {
-            this.infoSection.textContent = `Number ${rand} popped up. Press the button to try again!`;
-            this.displayButton.textContent = "Try Again ?";
+            textElement.textContent = `Number ${numberGenerated} popped! Try again ?`;
+            this.textContent = "Try again?";
         }
     }
+};
 
+const createDiceGame = (buttonId, textId) => {
+    let tries = 0;
+    const buttonElement = document.getElementById(buttonId);
+    const textElement = document.getElementById(textId);
+
+    buttonElement.textContent = "Click me!";
+    buttonElement.addEventListener("click", handleButtonClick(tries, textElement));
 }
 
-const s = new DiceGame("click_me", "text");
-
-console.log(s);
+createDiceGame("click_me", "text");
