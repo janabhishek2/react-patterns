d75fb3eba801ee97fb3cfbd80691404914e6eca4
Abhishek Jan <janabhishek2@gmail.com>
2024-08-17 22:57:25 +0530

    Handling errors in fetch

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,9 +1,17 @@
 const todoApi = `https://jsonplaceholder.typicode.com/todos`;
 
+// fetch never rejects the promise; except when network is down!
+// we need to check if promise gave positive status code using ok flag.
 const fetchTodos = async function() {
-    const response = await fetch(todoApi);
-    const data = await response.json();
-    console.log(data);
+    try{
+        const response = await fetch(todoApi);
+        if(!response.ok) {
+            throw new Error("Error found!");
+        }
+        const data = await response.json();
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 fetchTodos();
