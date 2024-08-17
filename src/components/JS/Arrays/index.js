9977f913abab626c343caa02a547f6e06ae5280c
Abhishek Jan <janabhishek2@gmail.com>
2024-08-17 15:08:04 +0530

    Flipped function

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,60 +1,11 @@
-// const toUppercase = str => str.toUpperCase();
-// const split = str => str.split(" ");
-
-// const strr = "i fall for her ";
-// const composed = compose(split, toUppercase);
-// const ans = composed(strr);
-
-// console.log(ans);
-// return;
-
-
-const partial = (func, ...fixed) => {
-    return function(...rem) {
-        return func(...fixed, ...rem);
+const flip = function(func) {
+    return function(...args) {
+        return func(...args.reverse())
     }
 };
 
-const compose = (...funcs) => {
-    return function(data) {
-        return funcs.reduceRight((value, func) => {
-            return func(value);
-        }, data)
-    }
-}
+const subtract = (a, b) => a - b;
+const flippedSubtract = flip(subtract);
 
-const getRandomRoll = () => Math.ceil(Math.random() * 6);
-
-// writing pure functions
-const isWon = (num) => num === 6;
-
-const handleButtonClick = function(tries, textElement) {
-    return function() {
-        const numberGenerated = getRandomRoll();
-        ++tries;
-        if(isWon(numberGenerated)) {
-            textElement.textContent = `You won after ${tries} tries!`;
-            tries = 0;
-            // this gets access to buttonElement that triggered this event
-            this.textContent = "Click me!"
-        } else {
-            textElement.textContent = `Number ${numberGenerated} popped! Try again ?`;
-            this.textContent = "Try again?";
-        }
-    }
-};
-
-const createDiceGame = (buttonId, textId) => {
-    let tries = 0;
-    const buttonElement = document.getElementById(buttonId);
-    const textElement = document.getElementById(textId);
-
-    buttonElement.textContent = "Click me!";
-    const argsBoundHandler = partial(handleButtonClick, tries, textElement);
-
-    buttonElement.addEventListener("click", argsBoundHandler());
-}
-
-const boundDiceGame = partial(createDiceGame, "click_me", "text");
-
-boundDiceGame();
+const ans = flippedSubtract(3, 4);
+console.log(ans);
