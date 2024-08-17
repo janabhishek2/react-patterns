41856a7bc14687acdc4230e478cbb1afce5a144f
Abhishek Jan <janabhishek2@gmail.com>
2024-08-17 16:03:58 +0530

    stylePicker exercise

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,11 +1,63 @@
-const flip = function(func) {
-    return function(...args) {
-        return func(...args.reverse())
+function compose(...functions) {
+    return function (result) {
+      return functions.reduceRight(function (prevResult, fn) {
+        return fn(prevResult);
+      }, result);
+    };
+  }
+  
+  // Your code goes here!
+  
+  const setSelectedFontSize = function(elementToApplyStyles) {
+      const fontElement = document.getElementById("font-size");
+      elementToApplyStyles.style.fontSize = fontElement.value;
+      return elementToApplyStyles;
+  }
+  
+  const setSelectedColor = function(element) {
+    const selectedColor = document.getElementById("color-picker");
+    element.style.color = selectedColor.value;
+    return element;
+  }
+  
+  const setUpperCase = function(element) {
+    const upperCaseElement = document.getElementById("uppercase");
+    if(upperCaseElement.checked) {
+      element.textContent = element.textContent.toUpperCase();
     }
-};
-
-const subtract = (a, b) => a - b;
-const flippedSubtract = flip(subtract);
-
-const ans = flippedSubtract(3, 4);
-console.log(ans);
+    return element;
+  }
+  
+  const setFontFamily = function(element) {
+    const fontFamilyElement = document.getElementById("font-family");
+    element.style.fontFamily = fontFamilyElement.value;
+    return element;
+  }
+  
+  const setText = function(element) {
+    const textElement = document.getElementById("input-text");
+    element.textContent = textElement.value;
+    return element;
+  }
+  
+  const handleClick = function() { 
+    const ouptutElement = document.getElementById("styled-text");
+  
+    const newElement = document.createElement("div");
+  
+    // font size
+  
+    const composedFunction = compose(setSelectedFontSize, setUpperCase, setFontFamily, setSelectedColor, setText);
+    const element = composedFunction(newElement);
+  
+    // remove all children of output element
+    for(let child of ouptutElement.children) {
+      child.remove();
+    }
+  
+    // append the new element
+    ouptutElement.appendChild(element);
+  }
+  
+  const buttonElement = document.getElementById("apply-styles");
+  buttonElement.addEventListener("click", handleClick);
