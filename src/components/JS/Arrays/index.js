c574b23f5aba262bacc1efd1854029532ea4399b
Abhishek Jan <janabhishek2@gmail.com>
2024-08-18 16:50:48 +0530

    Posts: jsonplaceholder exercise

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,38 +1,100 @@
-// Files selection with code
+// fetch the posts
 
-const filesInput = document.getElementById("myFiles");
+const fetchPostsUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-filesInput.addEventListener("change", (event) => {
-    const files = event.target.files;
-    uploadFiles(files);
-});
+// posts section div
+const postsDiv = document.getElementById("posts");
 
-const fileBaseUrl = "https://jsonplaceholder.typicode.com/todos";
+// create a post
+const createPost = function(post) {
+    const postElement = document.createElement("div");
+    const postContent = document.createElement("div");
+    postElement.className = "post";
+    postContent.className = "post-content";
 
-const uploadFiles = async function(formData) {
-    const headers = new Headers({
-        fileInput: true
-    });
+    postElement.appendChild(postContent);
 
-    try {
-        const response = await fetch(fileBaseUrl, {
-            headers,
-            method: "POST",
-            body: formData,
-        });
+    const postTitle = document.createElement("div");
+    postTitle.className = "post-title";
+    postTitle.textContent = post.title;
+
+    const postBody = document.createElement("div");
+    postBody.className = "post-body";
+    postBody.textContent = post.body;
+
+    postContent.appendChild(postTitle)
+    postContent.appendChild(postBody);
+
+    return postElement;
+}
+
+const populateUI = function(posts = []) {
+    // clear the posts section div chilren
+
+    for(let child of postsDiv.children) {
+        child.remove();
+    }
+
+    // insert the elements
+    for(let post of posts) {
+        const postElement = createPost(post);
+        postsDiv.appendChild(postElement);
+    }
+    
+    // return;
+    return;
+}
+
+const fetchPosts = async function() {
+    try{
+        const response = await fetch(fetchPostsUrl);
         if(!response.ok) {
-            throw new Error('failed to fetch!!');
+            throw new Error("Failed to fetch!");
         }
         const data = await response.json();
-        console.log(data);
-    } catch(er) {
-        console.log(er.message);
+        populateUI(data);
+    } catch(err) {
+        console.log(err.message);
     }
 }
 
-const filesInputElement = document.getElementById("myFiles");
-filesInputElement.addEventListener("change", (e) => {
-    const fd = new FormData();
-    fd.append("logo", filesInput.files[0]);
-    uploadFiles(fd);
-})
+const createAndInsertPost = function(post) {
+    // insert post at beginning of list
+
+    const postElement = createPost(post);
+    postsDiv.insertAdjacentElement("afterbegin", postElement);
+
+    return;
+};
+
+const insertPost = async function(post) {
+    try {
+        const payload = post;
+        const response = await fetch(fetchPostsUrl, {
+            body: JSON.stringify(payload),
+            method: "post"
+        });
+        if(!response.ok) throw new Error("Failed to fetch!");
+
+        createAndInsertPost(payload);
+
+    } catch(err) {
+        console.log(err.message);
+    }
+}
+fetchPosts();
+
+// add post event listener
+
+const addPostButton = document.getElementById("add-post");
+
+addPostButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    const postTitle = document.getElementById("new-post-title").value;
+    const postBody = document.getElementById("new-post-body").value;
+    const post = {
+        title: postTitle,
+        body: postBody
+    };
+    createAndInsertPost(post);
+});
