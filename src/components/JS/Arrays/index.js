d1a23d7b029046d64bd7b1f0fea74232fc605cc9
Abhishek Jan <janabhishek2@gmail.com>
2024-08-19 09:49:54 +0530

    SessionStorage: to show alert on webpage

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,7 +1,18 @@
 // SessionStorage
 
-// Session storage persists data for the session.
+// Session storage persists data for the session. ( per tab basis )
 // localStorage persist data for session/tabs/windows and binds it to the domain and protocol.
-// sessionStorage has same api as localStorage
+// sessionStorage has same api as localStorage, same size as localStorage ... approx 5MB
 
-console.log(sessionStorage);
+// Example: show alert banner only once in a session/tab
+
+const SHOW_ALERT_KEY = 'SHOW_ALERT_KEY'
+const showAlert = function() {
+    // this message shows only once when user opens the tab once.
+    if(!sessionStorage.getItem(SHOW_ALERT_KEY)) {
+        alert("We are shutting down the website!!");
+    }
+    sessionStorage.setItem(SHOW_ALERT_KEY, "true");
+}
+
+showAlert();
