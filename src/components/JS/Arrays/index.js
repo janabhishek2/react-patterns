3c6637cb01eeaa6739daa4b56e28726d89d5e394
Abhishek Jan <janabhishek2@gmail.com>
2024-08-19 09:38:50 +0530

    sessionStorage basics

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,66 +1,7 @@
-const getLocalStorageNotes = function() {
-    const notesString = localStorage.getItem("notes");
-    const notes = JSON.parse(notesString) ?? [];
-    return notes;
-}
+// SessionStorage
 
-const createNote = function(note) {
-    const noteElement = document.createElement("li");
-    noteElement.textContent = note;
-    return noteElement;
-}
+// Session storage persists data for the session.
+// localStorage persist data for session/tabs/windows and binds it to the domain and protocol.
+// sessionStorage has same api as localStorage
 
-const populateUI = function() {
-    const notes = getLocalStorageNotes();
-
-    // check for ul element
-    const ulElement = document.getElementById("notesContainer");
-    
-    // clear ul element children
-    const ulChildren = ulElement.children;
-    while(ulChildren.length !== 0) {
-        ulChildren[0].remove();
-    }
-
-    // populate notes
-    for(let note of notes) {
-        const noteElement = createNote(note);
-        ulElement.appendChild(noteElement);
-    }
-
-    return;
-}
-
-const addNote = function(note) {
-    let localStorageNotes = getLocalStorageNotes();
-    localStorageNotes.push(note);
-    const newNotesString = JSON.stringify(localStorageNotes);
-    localStorage.setItem("notes", newNotesString);
-
-    // populate UI
-    populateUI();
-}
-
-populateUI();
-
-const handleAddNote = function(e) {
-    e.preventDefault();
-    // get note data
-    const textAreaElement = document.getElementById("notesInput");
-    const newNote = textAreaElement.value;
-    addNote(newNote);
-}
-
-// create click handler for notes
-
-const saveNoteButton = document.getElementById("saveNote");
-saveNoteButton.addEventListener("click", handleAddNote);
-
-// Listen for the storage event; to re-populate the UI
-
-window.addEventListener("storage", (e) => {
-    console.log(e);
-    if(e.key === "notes") {
-        populateUI();
-    }
-});
+console.log(sessionStorage);
