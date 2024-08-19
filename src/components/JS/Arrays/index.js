1958ace3506f1d48d3f0682fdc7e9a6394429515
Abhishek Jan <janabhishek2@gmail.com>
2024-08-19 16:55:29 +0530

    indexedDB: basics

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,27 +1,54 @@
-// Saving all values of form to sessionStorage
+// IndexedDB
+// Is a type of sql database --> has objectStores in place of tables..; supports indexes
+// can store data > 5MB... on clients machine
 
-const checkoutForm = document.getElementById("checkoutForm");
+// open represents the connection to DB
+const open = indexedDB.open("my-first-db", 1);
 
-const form_key = "form_1"
-checkoutForm.addEventListener("input", (e) => {
-    // get sessioStorage object for form
-
-    const formValues = JSON.parse(sessionStorage.getItem(form_key)) ?? {};
-    const { name, value } = e?.target ?? {};
-    formValues[name] = value;
-
-    // set the values to sessionStorage
-    sessionStorage.setItem(form_key, JSON.stringify(formValues));
-});
-
-const populateForm = function() {
-    const formValues = JSON.parse(sessionStorage.getItem(form_key)) ?? {};
-    for(let key in formValues) {
-        // find the element in UI
-        const element = document.getElementById(key);
-        element && (element.value = formValues[key]);
-    }
-    return;
+open.onupgradeneeded = function() {
+    console.log('ru');
+    const db = open.result;
+    db.createObjectStore("MyUserStore", {
+        keyPath: "id"
+    });
 }
 
-populateForm();
+// called when the connection to db is successful
+open.onsuccess = async function() {
+    console.log("success");
+
+    // get the db result
+    const db = open.result;
+
+    // define the transaction between table.
+    const transaction = db.transaction("MyUserStore", "readwrite");
+
+    // get access to table/store
+    const store = transaction.objectStore("MyUserStore");
+
+    // store.put({
+    //     id: 1,
+    //     userName: "Abhishek",
+    //     age: 25
+    // });
+    
+    // put method
+    // store.put({
+    //     id: "2",
+    //     userName: "test",
+    // })
+
+    // get access to data in store
+
+    const data = store.get("2");
+    // gets called when data is retreived
+    data.onsuccess = function() {
+        const result = data.result;
+        console.log(result);
+    }
+};
+
+// called when the connection to db failed
+open.onerror = function() {
+    console.log("error");
+}
