072b8ec00c64b16c937dc872d8264a2e216a0ec5
Abhishek Jan <janabhishek2@gmail.com>
2024-08-19 08:13:03 +0530

    Notes app with localStorage

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,25 +1,57 @@
-// Toggle switch
-
-const modeSwitch = document.getElementById("switch__mode");
-
-function handleToggle() {
-    const current = localStorage.getItem('mode');
-    if(!current || current === "light-mode") {
-        localStorage.setItem('mode', 'dark-mode');
-        renderCorrectUI("dark-mode");
-    } else {
-        localStorage.setItem('mode', 'light-mode');
-        renderCorrectUI("light-mode");
-    }
-};
-
-function renderCorrectUI(mode) {
-    if(mode==='dark-mode'){
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
-    }
+const getLocalStorageNotes = function() {
+    const notesString = localStorage.getItem("notes");
+    const notes = notesString ? JSON.parse(notesString) : [];
+    return notes;
 }
 
-modeSwitch.addEventListener("click", handleToggle);
-renderCorrectUI(localStorage.getItem("mode"));
+const createNote = function(note) {
+    const noteElement = document.createElement("li");
+    noteElement.textContent = note;
+    return noteElement;
+}
+
+const populateUI = function() {
+    const notes = getLocalStorageNotes();
+
+    // check for ul element
+    const ulElement = document.getElementById("notesContainer");
+    
+    // clear ul element children
+    const ulChildren = ulElement.children;
+    while(ulChildren.length !== 0) {
+        ulChildren[0].remove();
+    }
+
+    // populate notes
+    for(let note of notes) {
+        const noteElement = createNote(note);
+        ulElement.appendChild(noteElement);
+    }
+
+    return;
+}
+
+const addNote = function(note) {
+    let localStorageNotes = getLocalStorageNotes();
+    localStorageNotes.push(note);
+    const newNotesString = JSON.stringify(localStorageNotes);
+    localStorage.setItem("notes", newNotesString);
+
+    // populate UI
+    populateUI();
+}
+
+populateUI();
+
+const handleAddNote = function(e) {
+    e.preventDefault();
+    // get note data
+    const textAreaElement = document.getElementById("notesInput");
+    const newNote = textAreaElement.value;
+    addNote(newNote);
+}
+
+// create click handler for notes
+
+const saveNoteButton = document.getElementById("saveNote");
+saveNoteButton.addEventListener("click", handleAddNote);
