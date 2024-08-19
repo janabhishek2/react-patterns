18932a7bdc4ff90c8897a0badcf010d8fc5bb1c7
Abhishek Jan <janabhishek2@gmail.com>
2024-08-19 10:36:33 +0530

    Session storage to persist search value

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,18 +1,12 @@
-// SessionStorage
+const searchElement = document.getElementById("searchInput");
+const searchKey = "search";
 
-// Session storage persists data for the session. ( per tab basis )
-// localStorage persist data for session/tabs/windows and binds it to the domain and protocol.
-// sessionStorage has same api as localStorage, same size as localStorage ... approx 5MB
+searchElement.addEventListener("input", (e) => {
+    sessionStorage.setItem(searchKey, e.target.value);
+});
 
-// Example: show alert banner only once in a session/tab
-
-const SHOW_ALERT_KEY = 'SHOW_ALERT_KEY'
-const showAlert = function() {
-    // this message shows only once when user opens the tab once.
-    if(!sessionStorage.getItem(SHOW_ALERT_KEY)) {
-        alert("We are shutting down the website!!");
-    }
-    sessionStorage.setItem(SHOW_ALERT_KEY, "true");
+const populateSearch = function() {
+    searchElement.value = sessionStorage.getItem(searchKey) ?? "";
 }
 
-showAlert();
+populateSearch();
