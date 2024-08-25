ff300bc7ca123e87ed63aaf6cd390371a6070396
Abhishek Jan <janabhishek2@gmail.com>
2024-08-25 10:00:37 +0530

    Geolocation: comments

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -7,6 +7,7 @@ const locationDisplay = document.getElementById("locationDisplay");
 
 getLocationButton.addEventListener("click", function() {
     if(navigator.geolocation) {
+        // getCurrentPosition is the api used to fetch the user permission
         navigator.geolocation.getCurrentPosition(function(position) {
             const { latitude, longitude } = position.coords;
             locationDisplay.textContent = `Latitude is ${latitude} and longitude is ${longitude}`;
