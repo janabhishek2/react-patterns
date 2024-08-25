19a2d50918a1f5e79b8a68baf9cb07e70d48413b
Abhishek Jan <janabhishek2@gmail.com>
2024-08-25 09:57:33 +0530

    navigator.geolocation api

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,59 +1,17 @@
-// IndexedDB
-// Is a type of sql database --> has objectStores in place of tables..; supports indexes
-// can store data > 5MB... on clients machine
+// Geolocation
 
-// open represents the connection to DB
-const open = indexedDB.open("my-first-db", 1);
+const geolocation = navigator.geolocation;
 
-open.onupgradeneeded = function() {
-    console.log('ru');
-    const db = open.result;
-    db.createObjectStore("MyUserStore", {
-        keyPath: "id"
-    });
-}
+const getLocationButton = document.getElementById("getlocation");
+const locationDisplay = document.getElementById("locationDisplay");
 
-// called when the connection to db is successful
-open.onsuccess = async function() {
-    console.log("success");
-
-    // get the db result
-    const db = open.result;
-
-    // define the transaction between table.
-    const transaction = db.transaction("MyUserStore", "readwrite");
-
-    // get access to table/store
-    const store = transaction.objectStore("MyUserStore");
-
-    // store.put({
-    //     id: 1,
-    //     userName: "Abhishek",
-    //     age: 25
-    // });
-    
-    // put method
-    // store.put({
-    //     id: "2",
-    //     userName: "test",
-    // })
-
-    // get access to data in store
-
-    const data = store.get("2");
-    // gets called when data is retreived
-    data.onsuccess = function() {
-        const result = data.result;
-        console.log(result);
+getLocationButton.addEventListener("click", function() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const { latitude, longitude } = position.coords;
+            locationDisplay.textContent = `Latitude is ${latitude} and longitude is ${longitude}`;
+        }, function(err) {
+            locationDisplay.textContent = `Error: ${err.message}`;
+        })
     }
-
-    // close the db after use
-    transaction.oncomplete = function() {
-        db.close()
-    }
-};
-
-// called when the connection to db failed
-open.onerror = function() {
-    console.log("error");
-}
+});
