81a3bb503db7403b2cbdf9d85e1bf7e457db67ec
Abhishek Jan <janabhishek2@gmail.com>
2024-08-26 10:01:41 +0530

    Tracking adViewTime

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,18 +1,25 @@
 // Intersection observer: api which can track when an element gets in and out of the viewport
 
+let adVisibleStartTime = undefined;
+let adVisibleTimes = [];
+
 const observer = new IntersectionObserver(function(entries) {
     entries.forEach(function(entry) {
-        const percentage = Math.round(entry.intersectionRatio * 100, 2);
-        if(entry.isIntersecting) {
-            console.log(`${percentage}% of the ad is visible !`);
-        } else {
-            console.log('Ad is not visible!');
+        const { isIntersecting } = entry;
+        if(isIntersecting) {
+            adVisibleStartTime = Date.now();
+        } else if(adVisibleStartTime) {
+            const viewedTime = Date.now() - adVisibleStartTime;
+            adVisibleTimes.push(viewedTime);
+            adVisibleStartTime = undefined;  
         }
+        console.log(adVisibleTimes);
+
     })
 }, {
-    // This property triggers the above callback when % of the element is visible.
-    threshold: [0, 0.25, 0.5, 0.75, 1]
+    threshold: 0.5
 });
 
-const adElement = document.getElementsByClassName("ad")[0];
-observer.observe(adElement);
+const ad = document.querySelector(".ad");
+observer.observe(ad);
+
