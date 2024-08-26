516fad37b3cb0d95f1817db2d72f61b7713d63ea
Abhishek Jan <janabhishek2@gmail.com>
2024-08-26 12:37:34 +0530

    multiple observers

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,27 +1,20 @@
 // Intersection observer: api which can track when an element gets in and out of the viewport
 
-let adVisibleStartTime = undefined;
-let adVisibleTimes = [];
-let observer = null;
-
-(() => {
-    let start = 0;
-    observer = new IntersectionObserver(function(entries) {
-        entries.forEach(function(entry) {
-            const { isIntersecting } = entry;
-            if(isIntersecting) {
-                start = Date.now();
-            } else if(start) {
-                const viewedTime = Date.now() - start;
-                adVisibleTimes.push(viewedTime);
-            }
-        });
-        console.log(adVisibleTimes);
-    }, {
-        threshold: 0.5
+const observer = new IntersectionObserver(function(entries) {
+    entries.forEach(function(entry) {
+        const { isIntersecting } = entry;
+        const { id } = entry.target;
+        if(isIntersecting) {
+            console.log(`${id} ad is visible!`);
+        } else {
+            console.log(`${id} ad is not visible`);
+        }
     });
-})()
+}, {
+    threshold: 0.5
+});
 
-const ad = document.querySelector(".ad");
-observer.observe(ad);
-
+const divs = document.querySelectorAll(".ad");
+divs.forEach(function(div) {
+    observer.observe(div);
+});
