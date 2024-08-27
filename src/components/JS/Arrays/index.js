20cf1080338de44ebafa6200eaef3a2d021a1776
Abhishek Jan <janabhishek2@gmail.com>
2024-08-27 19:14:22 +0530

    getUserMedia api for getting user media

--- src/components/JS/Arrays/index.js
+++ src/components/JS/Arrays/index.js
@@ -1,23 +1,25 @@
-// Lazy load images
+// getUserMedia
 
-const observer = new IntersectionObserver(function(entries) {
-    entries.forEach(async function(entry) {
-        if(entry.isIntersecting) {
-            try{
-                const response = await fetch('https://loremflickr.com/200/200');
-                entry.target.src = response.url;
-                observer.unobserve(entry.target);
-            }
-            catch(e){ 
-                console.log(e);
-            }
+const startStreamButton = document.getElementById("startStreamButton");
+
+startStreamButton.addEventListener("click", async function() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        });
+        if(!document.querySelector("video")) {
+            const videoElement = document.createElement("video");
+            videoElement.setAttribute("height", "100%");
+            videoElement.setAttribute("zIndex", 2);
+            videoElement.srcObject = stream;
+            videoElement.style.margin = "auto";
+            videoElement.setAttribute("width", "50%")
+            const container = document.getElementsByClassName("container")[0];
+            container.appendChild(videoElement);
         }
-    });
-}, {
-    threshold: 0.5
-});
 
-const images = document.querySelectorAll("img.lazy");
-for(let image of images) {
-    observer.observe(image);
-}
+    } catch(err) {
+        console.log(err);
+    }
+});
