318272f31a699b31c9d08409f6b931970843593d
Abhishek Jan <janabhishek2@gmail.com>
2024-09-30 07:24:06 +0530

    Using the useDeleteTodoMutation

--- src/components/rtk-query/components/TodoItem/TodoItem.jsx
+++ src/components/rtk-query/components/TodoItem/TodoItem.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLazyGetTodoQuery } from "../../slices/apiSlice";
+import { useDeleteTodoMutation } from "../../slices/deleteTodoSlice";
 
 const statusGetter = (completed) => {
         return completed ? "Completed..": "Not complete";
@@ -9,12 +10,20 @@ function TodoItem({ data }) {
     const { todo, id } = data;
     const [trigger, { data: todoData = {}, isLoading, isError, error = {} }] = useLazyGetTodoQuery();
 
+    const [deleteTodoFn, deleteTodoResult] = useDeleteTodoMutation();
+
+    useEffect(() => {
+        if(deleteTodoResult.isSuccess) {
+            alert("Successfully deleted the todo!");
+        }
+    }, [deleteTodoResult.isSuccess]);
+
     const handleGetStatus = function () {
         trigger("asd");
     };
 
     const handleDelete = function() {
-        
+        deleteTodoFn(id);
     }
    
     return (
