b2e9d596c4d36f175d8a76c8775026f4256bb699
Abhishek Jan <janabhishek2@gmail.com>
2024-10-02 16:34:57 +0530

    pre-fetch todo:2 data

--- src/components/rtk-query/RtkWrapper.jsx
+++ src/components/rtk-query/RtkWrapper.jsx
@@ -2,13 +2,19 @@ import React, { useState } from 'react'
 import { Provider } from 'react-redux';
 import Todo from './components/Todos';
 import store from './store/index';
+import apiSlice from './slices/apiSlice';
 
 function RtkWrapper() {
   const [showTodos, setShowTodos] = useState(false);
 
   const handleShowTodos = function() {
     setShowTodos(!showTodos);
-  }
+  };
+
+  // pre-fetch todo/2 data
+
+  store.dispatch(apiSlice.endpoints.getTodo.initiate(2));
+
   return (
     <Provider store={store}>
         {
