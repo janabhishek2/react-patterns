d0228eeade691970f094298418f218afc6f68857
Abhishek Jan <janabhishek2@gmail.com>
2024-10-27 19:51:50 +0530

    Refactoring

---
+++ packages/react-stories/stories/Testing/Test.stories.js
@@ -0,0 +1,17 @@
+// import { Button } from "@react-patterns/react/src/index";
+import { Button } from "@react-patterns/react/src/index";
+
+console.log(Button);
+
+import React from 'react';
+
+export default {
+    title: 'Button from DS',
+    component: Button
+};
+
+const Button1 = () => <Button>Hello</Button>;
+
+export {
+    Button1
+};
