dfcce7a0b5a9e87977bae26eac4cbf070fee07d8
Abhishek Jan <janabhishek2@gmail.com>
2024-10-29 17:45:42 +0530

    color spacing changes

--- packages/react/src/atoms/Color/Color.jsx
+++ packages/react/src/atoms/Color/Color.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import '@react-patterns/scss/lib/Utilities.css';
 import PropTypes from 'prop-types';
+import { SPACING } from '../../foundation/Spacing';
 
 function Color(props) {
-    const { hexCode, width = '1rem', height= '1rem' } = props;
+    const { hexCode, width = "sm", height= "sm"} = props;
     return (
-        <div className="dse-border-radius-sm" style={{
-            width,
-            height,
+        <div className={`dse-width-${width} dse-height-${height}`} style={{
             backgroundColor: hexCode
         }}></div>
     )
 }
 
 Color.propTypes = {
-    hexCode: PropTypes.oneOf(["#000", "#fff", "#ddd"]),
-    width: PropTypes.string,
-    height: PropTypes.string
+    hexCode: PropTypes.string,
+    width: PropTypes.oneOf(Object.keys(SPACING)),
+    height:  PropTypes.oneOf(Object.keys(SPACING))
 }
 
 export default Color
