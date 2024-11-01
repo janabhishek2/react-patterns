20addc66741c1d5a20e83bdc6447115e36e2d321
Abhishek Jan <janabhishek2@gmail.com>
2024-11-01 16:01:55 +0530

    basic implementation of selected option

--- packages/react/src/molecules/Select/Select.jsx
+++ packages/react/src/molecules/Select/Select.jsx
@@ -1,57 +1,83 @@
-import React, { useEffect, useRef, useState } from 'react';
-import PropTypes from 'prop-types';
-import '@react-patterns/scss/lib/CustomSelect.css';
-import Down from './Down';
-import Up from './Up';
+import React, { useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import "@react-patterns/scss/lib/CustomSelect.css";
+import Down from "./Down";
+import Up from "./Up";
+import Tick from "./Tick";
 
 const Select = (props) => {
     const { onOptionSelect: optionSelectCallback, options, label } = props;
 
-    const onOptionSelect = (option, index) => {
-        optionSelectCallback(option, index);
-    };
+    /* States for the Select */
 
     const [open, setIsOpen] = useState(false);
     const triggerbuttonRef = useRef(null);
     const [triggerButtonHeight, setTriggerButtonHeight] = useState(0);
+    const [selectedOption, setSelectedOption] = useState({});
 
-    console.log(triggerButtonHeight);
+    /* Event handlers */
+
+    const onOptionSelect = (option, index) => {
+        setSelectedOption(option);
+        optionSelectCallback(option, index);
+    };
+
     const toggleOpen = () => {
         setIsOpen(!open);
-    }
+    };
 
     useEffect(() => {
-        if(triggerbuttonRef?.current) {
+        if (triggerbuttonRef?.current) {
             setTriggerButtonHeight(triggerbuttonRef?.current?.offsetHeight);
         }
     }, [triggerbuttonRef?.current]);
 
     return (
-        <div className='dse-select'>
-            <button ref={triggerbuttonRef} className='dse-select__label' onClick={toggleOpen}>
+        <div className="dse-select">
+            <button
+                ref={triggerbuttonRef}
+                className="dse-select__label"
+                onClick={toggleOpen}
+            >
+                <span>{label}</span>
                 <span>
-                    {label}
-                </span>
-                <span>
-                    {
-                        open ? (<Up width="1rem" height="1rem" />): (<Down width="1rem" height="1rem" />)
-                    }
+                    {open ? (
+                        <Up width="1rem" height="1rem" />
+                    ) : (
+                        <Down width="1rem" height="1rem" />
+                    )}
                 </span>
             </button>
-            {
-                open && (
-                    <ul className='dse-select__overlay' style={{
-                        top: triggerButtonHeight
-                    }}>
-                        {
-                            options.map((option, index) => {
-                                const { label, value } = option;
-                                return <li className='dse-select__list-item' onClick={() => onOptionSelect(option, index)} key={value}>{label}</li>
-                            })
-                        }
-                    </ul>
-                )
-            }
+            {open && (
+                <ul
+                    className="dse-select__overlay"
+                    style={{
+                        top: triggerButtonHeight,
+                    }}
+                >
+                    {options.map((option, index) => {
+                        const { label, value } = option;
+                        return (
+                            <li
+                                className={
+                                    selectedOption?.value === value
+                                        ? "dse-select__list-item--selected"
+                                        : "dse-select__list-item"
+                                }
+                                onClick={() => onOptionSelect(option, index)}
+                                key={value}
+                            >
+                                <span>{label}</span>
+                                {selectedOption?.value === value && (
+                                    <span>
+                                        <Tick width="1.5rem" height="1rem"  />
+                                    </span>
+                                )}
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
         </div>
     );
 };
@@ -59,13 +85,13 @@ const Select = (props) => {
 Select.propTypes = {
     onOptionSelect: PropTypes.func,
     options: PropTypes.array,
-    label: PropTypes.string
-}
+    label: PropTypes.string,
+};
 
 Select.defaultProps = {
     onOptionSelect: () => {},
     options: [],
-    label: "Select"
-}
+    label: "Select",
+};
 
 export default Select;
