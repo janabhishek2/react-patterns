ef131fa62cb8df5c1427dad087ebc4e876fc2904
Abhishek Jan <janabhishek2@gmail.com>
2024-11-03 09:24:59 +0530

    build updated

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,9 +1,18 @@
+
+/* Atoms import */
+
 import Button from "./atoms/Button";
 import Color from './atoms/Color';
+import Margin from './atoms/Margin';
+import Text from './atoms/Text';
+
+/* Molecules import */
 import Select from './molecules/Select';
 
 export {
     Button,
     Color,
-    Select
+    Select,
+    Margin,
+    Text
 };
