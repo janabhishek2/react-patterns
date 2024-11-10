21cdc0370be613adac10147f1d2373c9f8d5b431
Abhishek Jan <janabhishek2@gmail.com>
2024-11-10 09:33:44 +0530

    feat: added notes

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,42 +1,6 @@
-const curry = (func) => {
-  return function f1(...args) {
-    if(args.length >= func.length) {
-      // if num of args of calling function matches function provided to curry
-      return func(...args);
-    } else {
-      // return partially applied function
-      return function f2(...moreArgs) {
-        let newArgs = [...args, ...moreArgs];
-        // return f1 recursively
-        return f1(...newArgs);
-      }
-    }
-  }
-};
+let arr = [1, 2, 3];
+let element = 4;
 
-let items = [
-  {
-    id: 1,
-    name: "Abhishek"
-  },
-  {
-    id: 2,
-    name: "George"
-  },
-  {
-    id: 3,
-    name: "Styka"
-  }
-];
+arr.push(element);
 
-const curriedMap = curry((fn, items) => {
-  return items.map(fn);
-});
-
-// curry the map function to get all ids in an array ?
-
-let getIdsFunction = item => item?.id;
-const getIds = curriedMap(getIdsFunction);
-
-const ids = getIds(items);
-console.log(ids);
+console.log(arr);
