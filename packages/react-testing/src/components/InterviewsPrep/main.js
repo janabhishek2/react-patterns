3dff5d5b0cc41d0a576acf8ce56ebed81556cb67
Abhishek Jan <janabhishek2@gmail.com>
2024-11-10 11:12:42 +0530

    feat: check if user with a name exists

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,11 +1,14 @@
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
+let users = [
+    {
+        name: "John"
+    },
+    {
+        name: "Jack"
+    }
+];
 
-// creates new array does not modify existing arr
-const newArr = arr1.concat(arr2);
+const isNameExists = (name, users) => {
+    return users.some(user => user.name === name);
+};
 
-// new array is created in this case
-const newArr1 = [...arr1, ...arr2];
-
-// arr1 is modified
-// arr1.push(...arr2);
+console.log(isNameExists("John", users));
