ea547a9794083c49f51d31f0abd5c9653bfaa723
Abhishek Jan <janabhishek2@gmail.com>
2024-11-10 11:30:42 +0530

    feat: using includes and reduce

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -2,6 +2,11 @@
 
 const arr = [1,3,1,1,1,1,2];
 
-const newArr = [...new Set(arr)];
+// Using includes and reduce
+
+const newArr = arr.reduce((acc, element) => {
+    // reduce returns acc at each iteration of array
+   return acc.includes(element) ? acc : [...acc, element];
+}, []);
 
 console.log(newArr);
