9bac6de860d14b3203d6df042feb284f72b06fa6
Abhishek Jan <janabhishek2@gmail.com>
2024-12-09 13:42:52 +0530

    feat: extends using prototypes

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,14 +1,71 @@
-const employee = {
-    name: "Abhishek",
-    id: 123,
-    salary: 300
-};
+// method: using prototype funcitons
 
-const manager = {
-    department: "CSD"
+function Employee(name, id) {
+    this.name = name;
+    this.id = id;
 }
 
-manager.__proto__ = employee;
+Employee.prototype.getDetails = function() {
+    console.log(`Employee name is ${this.name} and id is ${this.id}`);
+}
+
+function Manager(params) {
+    Employee.call(this, ...arguments);
+}
+
+// first extend then add properties
+// this wont be added to prototype
+Manager.prototype.test = 'test';
+
+// Extend the employee class and re-assign the manager constructor
+Manager.prototype = Object.create(Employee.prototype);
+Manager.prototype.constructor = Manager;
+
+// add methods for Manager class
+Manager.prototype.getManagerDetails = function() {
+    console.log("Manager is", this.name);
+}
+const mgr = new Manager("abhishek", 123);
+console.log(mgr);
+
+
+
+// Manager.prototype = {
+//     ...Employee.prototype,
+//     ...Manager.prototype,
+//     constructor: Manager
+// }
+
+
+// const mgr = new Manager("Abhishek", 123);
+// console.log(mgr);
+
+// function Manager(params) {
+//     Employee.apply(this, arguments);
+//     console.log("This is manager constructor");
+// }
+
+// Manager.prototype.getManagerInfo = () => {
+//     console.log('Manager info');
+// }
+
+// Manager.prototype = Object.create(Employee.prototype);
+// Manager.prototype.constructor = Manager;
+
+// const mgr = new Manager("Bhakti Gujar", 333);
+// method: using prototypes
+
+// const employee = {
+//     name: "Abhishek",
+//     id: 123,
+//     salary: 300
+// };
+
+// const manager = {
+//     department: "CSD"
+// }
+
+// manager.__proto__ = employee;
 
 // class Employee {
 //     salary = null;
