1dba309c275a373aa8c1534436d9a1d09f450b47
Abhishek Jan <janabhishek2@gmail.com>
2024-12-21 15:16:18 +0530

    feat: basic replace

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,10 +1,4 @@
-const regex = /(\d{3})-(\d{4})/g;
+const s = "unicorns and rainbows";
+const newStr = s.replace(/unicorns/, 'cupcakes');
 
-const str = "111-2222 and 222-3344 and 444-2231 and 123-12332";
-
-let result = regex.exec(str);
-
-while(result !== null) {
-    console.log(result);
-    result = regex.exec(str);
-}
+console.log(newStr);
