23afdc918d0f8ba28c55d9697ef46f54facac04b
Abhishek Jan <janabhishek2@gmail.com>
2024-12-21 15:18:06 +0530

    feat: replace by capture groups

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,4 +1,5 @@
-const s = "unicorns and rainbows";
-const newStr = s.replace(/unicorns/, 'cupcakes');
+const s = "unicorns and rainbows and cupcakes";
+// $1 means the first capture group; $0 means whole capture group
+const newStr = s.replace(/([aeiou])/g, "$1$1");
 
 console.log(newStr);
