d424b8edfe5834cdda572b9684c0bbde5c7bfd28
Abhishek Jan <janabhishek2@gmail.com>
2024-12-22 16:52:00 +0530

    feat: 2 strings as anagrams

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,10 +1,30 @@
-// palindrome
+// anagram
 
-const isPalindrome = (str) => {
-    // O(n) solution
-    return str === str.split("").reverse().join("");
+const str1 = "listen";
+const str2 = "silent";
+
+const shallowCompare = (obj1, obj2) => {
+   const keys1 = Object.keys(obj1);
+   const keys2 = Object.keys(obj2);
+
+   if(keys1.length !== keys2.length) return false;
+
+   return keys1.every((key) => {
+    return obj1[key] === obj2[key]
+   })
+}
+const createHashing = (str) => {
+    const hashTable = {};
+    Array.from(str).forEach(item => {
+        if(item !== " "){
+            hashTable[item] = (hashTable?.[item] || 0) + 1;
+        }
+    })
+    return hashTable;
 };
 
-const str = "abcba";
-const ans = isPalindrome(str);
-console.log(ans);
+const hash1 = createHashing(str1);
+const hash2 = createHashing(str2);
+
+const isEqual = shallowCompare(hash1, hash2);
+console.log(isEqual);
