a6cda33b5a4347061e3f0abca43eb886b418109d
Abhishek Jan <janabhishek2@gmail.com>
2024-12-22 23:53:36 +0530

    feat: convert time to 24 hrs

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,10 +1,25 @@
-// string to title case
+// convert time from 12 to 24 hrs
 
-const convertToTitleCase = (str) => { 
-    return str.toLowerCase().split(" ").map(item => item[0].toUpperCase() + item.slice(1)).join(" ");
-}
+const convertTo24Hours = (time) => {
+    debugger;
+    const timeRegex = /(.*):(\d{1,2})(.*)/;
+    
+    const values = timeRegex.exec(time);
+    
+    let hour = Number(values[1]);
+    let min = Number(values[2]);
+    let timeOfDay = values[3];
 
-const str = "I'm a little tea pot";
-const ans = convertToTitleCase(str);
+    if(timeOfDay === "AM") {
+        hour = hour%12;
+    } else if(timeOfDay === "PM" && hour !== 12) {
+        hour = hour + 12;
+    }
+    return `${hour}:${min}`;
+    
+};
+
+const time = "10:02PM";
+const ans = convertTo24Hours(time);
 
 console.log(ans);
