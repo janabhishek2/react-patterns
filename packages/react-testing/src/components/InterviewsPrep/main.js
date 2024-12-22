fc6769d5dd3a63c7d514e5b1ef22ece02315c36a
Abhishek Jan <janabhishek2@gmail.com>
2024-12-22 12:32:38 +0530

    feat: use closure as cache

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,48 +1,22 @@
-// shallow compare objects
+// memoisation
 
-const typeOf = (object) => {
-    const typeofObjectString = Object.prototype.toString.call(object).toLowerCase();
-    const regex = /\[object (.*)\]/;
-    const type = regex.exec(typeofObjectString);
-    return type[1];
-}
+const input = 12;
 
-const deepCompare = (obj1, obj2) => {
-    if(typeOf(obj1) !== typeOf(obj2)) {
-        return false;
-    }
-    if(typeOf(obj1) === "object") {
-        if(Object.keys(obj1).length !== Object.keys(obj2).length) {
-            return false;
+const getValue = () => {
+    let cache = null;
+    return function(input) {
+        // if it is in cache then return computed value;
+        if(cache) {
+            return cache;
         }
-        return Object.keys(obj1).every(key => {
-            return deepCompare(obj1[key],obj2[key]);
-        });
-    }
-    if(typeOf(obj1) === "array") {
-        if(obj1.length !== obj2.length) {
-            return false;
-        }
-        return obj1.every((item, index) => deepCompare(item, obj2[index]));
-    }
-    if(typeOf(obj1) === "date") {
-        return obj1.getTime() === obj2.getTime()
-    }
-    return obj1 === obj2;
-}
-
-const o1 = {
-    a: 1,
-    b: {
-        c: 2
-    }
-};
-const o2 = {
-    a: 1, 
-    b: {
-        c: 2
+        // else set cache and return input+10
+        cache = input + 10;
+        return cache;
     }
 };
 
-const ans = deepCompare(o1, o2);
-console.log(ans);
+const computeValue = getValue();
+console.log(computeValue(80));
+console.log(computeValue(30));
+console.log(computeValue(40));
+console.log(computeValue(50));
