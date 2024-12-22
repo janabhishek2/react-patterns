86e9ef1b504cb81e2f8a6e2d616717c6581626e0
Abhishek Jan <janabhishek2@gmail.com>
2024-12-22 12:52:15 +0530

    feat: getFibonacchi memoised

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,12 +1,20 @@
 // fibonacci
 
-const getFib = (n) => {
-    if(n <= 1){
-        return n;
-    } else {
-        return getFib(n-1) + getFib(n-2);
+const fibonacchiNthTermGen = () => {
+    let cache = {};
+    return function Fib(n) {
+        if(n <= 1){
+            return n;
+        }
+        // if item is in cache then return it, else save it to cache and return it
+        else if(cache?.[n]) return cache?.[n];
+        else {
+            return cache[n] = Fib(n-1) + Fib(n-2);
+        }
     }
 }
 
-const fib4 = getFib(4);
-console.log(fib4);
+const getFib = fibonacchiNthTermGen();
+
+const fib1 = getFib(6);
+console.log(fib1);
