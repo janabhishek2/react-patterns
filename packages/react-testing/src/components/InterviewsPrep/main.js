f79f3e7781dfd8738094bfaae7da343e44e54f47
Abhishek Jan <janabhishek2@gmail.com>
2024-12-22 12:40:25 +0530

    feat: memoised value used

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -3,20 +3,21 @@
 const input = 12;
 
 const getValue = () => {
-    let cache = null;
+    // use variables of outer function to act like cache
+    let cache = {};
     return function(input) {
-        // if it is in cache then return computed value;
-        if(cache) {
-            return cache;
-        }
-        // else set cache and return input+10
-        cache = input + 10;
-        return cache;
+        debugger;
+       if(input in cache) {
+        return cache[input];
+       }
+       else {
+        return cache[input] = input + 10;
+       }
     }
 };
 
 const computeValue = getValue();
-console.log(computeValue(80));
+console.log(computeValue(30));
 console.log(computeValue(30));
 console.log(computeValue(40));
 console.log(computeValue(50));
