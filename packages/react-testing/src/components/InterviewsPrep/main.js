520f7045e6c194c0e09fbcb0efc282c9f6434cdc
Abhishek Jan <janabhishek2@gmail.com>
2024-12-24 12:02:38 +0530

    feat: fetch data using fetch

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,57 +1,16 @@
-// Object flattening
+// fetch api
 
-const typeOf = (object) => {
-    const regex = /\[object (.*)\]/;
-    const typeOfString = Object.prototype.toString.call(object).toLowerCase();
-    return regex.exec(typeOfString)[1];
-}
-const flattenObject = (object, outerKey = undefined) => {
-    let newObject = {};
-    if(outerKey) {
-        Object.keys(object).forEach(key => {
-            if(typeOf(object[key]) === "object") {
-                let tempObject = flattenObject(object[key], `${outerKey}.${key}`);
-                newObject = {
-                    ...newObject,
-                    ...tempObject
-                }
-            } else {
-                newObject[`${outerKey}.${key}`] = object[key];
-            }   
-        });
-        return newObject;
-    } else {
-        Object.keys(object).forEach(key => {
-            let temp = typeOf(object[key]);
-            if(typeOf(object[key]) === "object") {
-                let tempObject = flattenObject(object[key], key);
-                newObject = {
-                    ...newObject,
-                    ...tempObject
-                }
-            } else {
-                newObject[key] = object[key];
-            }   
-        });
-        return newObject;
-    }
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1';
+
+const getData = () => {
+    fetch(apiUrl)
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(response => {
+        console.log(response);
+    })
 };
 
-const o = { 
-    a: 1,
-    b: {
-        c: {
-            d: [2,3,4],
-            e: 5,
-        },
-        f: {
-            g: 6,
-            h: 7
-        },
-        i: 8
-
-    }
-};
-const flattenedObject = flattenObject(o);
-
-console.log(flattenedObject);
+getData();
