0dc85363178f949e42d3563f5b14ee406aa71b03
Abhishek Jan <janabhishek2@gmail.com>
2024-12-25 15:11:29 +0530

    feat: map users and user status using promises

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,22 +1,82 @@
-// parallel async funcitons
+// map data recevied from 2 promises;
 
-const asyncFunc = (callback) => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => callback(json))
+const users = [
+    {
+        id: 1,
+        name: "Abhishek"
+    },
+    {
+        id: 2,
+        name: "John"
+    }
+];
+
+const userStatus = [
+    {
+        id: 1,
+        isActive: true
+    },
+    {
+        id: 2,
+        isActive: false
+    }
+];
+
+const getUsers = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(users);
+        }, 1000);
+    })
 };
 
-const promisifyAndRemoveCallback = (asyncFunc) => {
+const getUserStatus = () => {
     return new Promise((resolve) => {
-        asyncFunc((data) => {
-            resolve(data);
+        setTimeout(() => {
+            resolve(userStatus);
+        }, 3000);
+    })
+};
+
+const getParallelRequests = (reqs) => {
+    const data = new Array(reqs.length);
+    let counter = 0;
+    return new Promise((resolve) => {
+        reqs.forEach(async (req, index) => {
+            const reqData = await req();
+            data[index] = reqData;
+            counter++;
+
+            if(counter >= reqs.length) {
+                resolve(data);
+            }
         });
     })
-}
-
-const test = async () => {
-    const newMethod = await promisifyAndRemoveCallback(asyncFunc);
-    console.log(newMethod);
 };
 
-test();
+const mapAndCombine = async () => {
+    try{
+        const [users, userStatus] = await getParallelRequests([getUserStatus, getUsers]);
+        let newUsers = {};
+        users.forEach(user => {
+            newUsers[user.id] = user;
+        });
+        let newUserStatus ={}
+        userStatus.forEach(userStatus => {
+            newUserStatus[userStatus.id] = userStatus;
+        });
+        let newData = [];
+        Object.keys(newUsers).forEach(userId => {
+            let data = {
+                ...newUsers[userId],
+                ...newUserStatus[userId]
+            };
+            newData.push(data);
+        });
+        console.log(newData);
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+mapAndCombine();
