e0c501233f57bda8e9239ada973059d8bcc9341d
Abhishek Jan <janabhishek2@gmail.com>
2024-12-27 17:53:59 +0530

    feat: highlight all words with length greater than 8

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,33 +1,22 @@
-// request manager implementation
+// Highlight all chars with length>=8 with yellow background.
 
-const fetchRequest = () => {
-    // Init delay of 1000ms
-    let delay = 0;
-    return function fetchInternal(url, attempts = 1) {
-        if(attempts > 0) {
-            setTimeout(() => {
-                return fetch(url, {
-                    method: "GET"
-                })
-                .then(res => res.json())
-                .catch((err) => {
-                    attempts = attempts - 1;
-                    delay += 1000;
-                    fetchInternal(url, attempts);
-                })
-            }, delay);
-        }
+// TODO
+// 1. gather the input from <p/> tag.
+// 2. Split it based on " ". Convert to array
+// 3. Check for individual words in array with length greater than 8.
+// 4. merge all with a span.
+
+const pElement = document.getElementsByTagName("p")[0];
+let pText = pElement.innerHTML;
+pElement.innerHTML = null;
+
+// manipulate data;
+let newSpanElements = pText.split(" ").map(item => {
+    const spanElement = document.createElement("span");
+    if(item.length >= 8) {
+        spanElement.style.backgroundColor = "yellow";
     }
-};
+    spanElement.innerText = `${item} `;
+    pElement.append(spanElement);
+});
 
-const func = async function() {
-    try{
-        let getRequest = fetchRequest();
-        const res = await getRequest('https://jsonplaceholder.typicode.com/todos', 10);
-        console.log(res);
-    } catch(err) {
-        console.log(err.message);
-    }
-};
-
-func();
