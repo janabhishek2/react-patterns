70357ef4f456e40385a701a5b4da0b90e3121498
Abhishek Jan <janabhishek2@gmail.com>
2025-02-17 23:52:59 +0530

    feat: reconciliation react

--- packages/react-testing/src/components/Reconicliliation/Input.jsx
+++ packages/react-testing/src/components/Reconicliliation/Input.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function Input({ hint }) {
+  console.log('input render');
   return (
     <input type="text" placeholder={hint} />
   )
 }
 
-export default Input
+export default React.memo(Input); 
--- packages/react-testing/src/components/Reconicliliation/Reconc.jsx
+++ packages/react-testing/src/components/Reconicliliation/Reconc.jsx
@@ -2,23 +2,29 @@ import React, { useState } from 'react'
 import Input from './Input';
 
 function Reconc() {
+    const data = [
+        {
+            hint: "one"
+        },
+        {
+            hint: "two"
+        }
+    ];
     const [toggle, setToggle] = useState(false);
 
-    return (
-        <div>
-            Reconciliation example
-            <br/>
-            <label htmlFor='checkBox'>Check me! </label>
-            <input type="checkbox" checked={toggle} name="checkBox" onChange={() => setToggle(toggle => !toggle)}/>
+    // if we not use key here then react will not knpw when it needs to re-mount the components, it will simply re-render.
+    const inputs = data.map(( item,index ) =>(<Input hint={item.hint} key={index} />));
+    
+    // re-mount -> state is reset and component is remounted.
+    toggle && inputs.reverse();
 
-            <br/>
-
-            {/* Input is not re-mounted when toggle is changed */}
-            {
-                toggle ? (<Input hint={"hint1"} />) : (<Input hint={"hint2"} />)
-            }
-        </div>
-    )
+    return <>
+        <label htmlFor='toggleCheck'>Click me</label>
+        <input type="checkbox" name="toggleCheck" onChange={() => setToggle(!toggle)} />
+        {
+            inputs
+        }
+    </>;
 }
 
 export default Reconc
