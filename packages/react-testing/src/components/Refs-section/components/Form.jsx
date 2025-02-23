e8b4d9b50598a349282116bcea3aae457cdaee66
Abhishek Jan <janabhishek2@gmail.com>
2025-02-23 20:44:42 +0530

    feat: refs basics

--- packages/react-testing/src/components/Refs-section/components/Form.jsx
+++ packages/react-testing/src/components/Refs-section/components/Form.jsx
@@ -4,7 +4,7 @@ export default function() {
     
     const ref = useRef("");
 
-    console.log('render');
+    const [test, setTest] = useState(false);
 
     const handleSubmit = () => {
         console.log(ref.current);
@@ -15,13 +15,19 @@ export default function() {
         ref.current = e.target.value;
     };
 
+    // length will be updated only when component will re-render due to any state change.
+    const length = ref.current?.length ?? 0;
+
     return (
         <>
-            <label for="name">
+            <label htmlFor="name">
                 Name: 
             </label>
             <input type="text" onChange={handleChange} />
             <button onClick={handleSubmit}>Submit</button>
+            <div/>
+            <button onClick={() => setTest(!test)}>update state</button>
+            <span>Length is: {length}</span>
         </>
         )
 }
