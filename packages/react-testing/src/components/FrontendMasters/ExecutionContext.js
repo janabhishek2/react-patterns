284b91a832f674053aa9a576c37c9a0452fa1f47
Abhishek Jan <janabhishek2@gmail.com>
2025-07-27 11:19:38 +0530

    web internals

--- packages/react-testing/src/components/FrontendMasters/ExecutionContext.js
+++ packages/react-testing/src/components/FrontendMasters/ExecutionContext.js
@@ -17,3 +17,8 @@ function demo() {
 // RAF --> CSS calculation --> Layout --> Paint.
 
 demo();
+
+// Browsers take 16ms to update the DOM after the code is run.
+// 60 frames are shown in 1000sec, 1 frame is shown in 1000/60 = 16ms.
+
+// if a function takes more than 12-14ms, then it is considered slow.
