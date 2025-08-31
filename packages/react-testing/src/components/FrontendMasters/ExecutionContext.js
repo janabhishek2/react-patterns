47828f99b8af4f907d8ad552da99469dbf07a7b8
Abhishek Jan <janabhishek2@gmail.com>
2025-08-31 20:55:29 +0530

    preserve value of this

--- packages/react-testing/src/components/FrontendMasters/ExecutionContext.js
+++ packages/react-testing/src/components/FrontendMasters/ExecutionContext.js
@@ -2,11 +2,12 @@
 function debounce(callback, delay) {
     let id = null;
     return function(...args) {
+        let self = this;
         if(id) {
             clearTimeout(id);
         }
         id = setTimeout(() => {
-            callback(...args);
+            callback.apply(self, args);
         }, delay)
     }
 }
@@ -15,9 +16,12 @@ const apiCallFn = (value) => {
     console.log("Api is called with", value);
 }
 
-const debouncedApiCall = debounce(apiCallFn, 2000);
+const o = {
+    a: 1,
+    debouncedApiCall: debounce(apiCallFn, 2000)
+}
 
 const handleChange = (e) => {
     const inputElement = document.getElementById("fname");
-    debouncedApiCall(inputElement.value);
+    o.debouncedApiCall(inputElement.value);
 }
