6627c7c2bca2aa87d8b2a537101716e506a09d38
Abhishek Jan <janabhishek2@gmail.com>
2025-08-31 20:19:06 +0530

    debounce: implemented

--- packages/react-testing/src/components/FrontendMasters/ExecutionContext.js
+++ packages/react-testing/src/components/FrontendMasters/ExecutionContext.js
@@ -1,34 +1,23 @@
-// Throttle using wait variable
-function throttle(func, wait, option = {leading: false, trailing: true}) {
-    let waiting = false;
-    let lastArgs = null;
-    return function wrapper(...args) {
-      if(!waiting) {
-        waiting = true;
-        // helper function to trigger a new waiting period
-        const startWaitingPeriod = () => setTimeout(() => {
-          // if at the end of the waiting period lastArgs exist, execute the function using it 
-          if(lastArgs) {
-            func.apply(this, lastArgs);
-            lastArgs = null;
-            // start another waiting period
-            startWaitingPeriod();
-          }
-          else {
-            waiting = false;
-          }
-        }, wait);
-        func.apply(this, args);
-        startWaitingPeriod();
-      }
-      else {
-        lastArgs = args; // store the arguments of the last function call within waiting period
-      }
-    }
-  }
 
-const scroll = () => {
-    console.log("Scroll call!");
+function debounce(callback, delay) {
+    let id = null;
+    return function(...args) {
+        if(id) {
+            clearTimeout(id);
+        }
+        id = setTimeout(() => {
+            callback(...args);
+        }, delay)
+    }
 }
 
-const callScroll = throttle(scroll, 2000);
+const apiCallFn = (value) => {
+    console.log("Api is called with", value);
+}
+
+const debouncedApiCall = debounce(apiCallFn, 2000);
+
+const handleChange = (e) => {
+    const inputElement = document.getElementById("fname");
+    debouncedApiCall(inputElement.value);
+}
