b0292bcb423a44ca771f3e7df65c0d2216d801d5
Abhishek Jan <abhishek.jan@brevo.com>
2025-09-26 10:27:49 +0530

    function declaration syntax

--- packages/react-testing/src/components/FrontendMasters/ExecutionContext.js
+++ packages/react-testing/src/components/FrontendMasters/ExecutionContext.js
@@ -1,51 +1,20 @@
-// Polyfill of set-timeout
+// Function variations
 
-// Use requestIdleCallback to mimic the functionality of setTimeout. It creates a low priority callback.
-//  The callback will be run when main thread is empty.
+// Named function expression
 
-window.timerId = 1231;
-window.setTimeout = function(callback, delay, ...args) {
-    const timerId = window.timerId++;
-    const time = Date.now() + delay;
-    window.timers = {
-        ...window.timers,
-        [timerId] : {
-            callback,
-            time,
-            args: [...args]
-        }
-    }
+// const x = function test() {
+//     console.log(test);
+// }
 
-    if(Object.keys(window.timers).length === 1) {
-        requestIdleCallback(processTimers);
-    }
-}
+// Function expression
+// const x = function() {
+//     console.log("hi");
+// }
 
-function processTimers() {
-    const executeTimer = function(timerId) {
-        const { callback, time, args } = window.timers[timerId];
-        if(Date.now() >= time) {
-            callback(...args)
-            delete window.timers[timerId];
-        } else {
-           requestIdleCallback(processTimers);
-        }
-    }
-    Object.keys(window.timers).forEach(executeTimer);
-};
+// Function declaration
 
-window.clearTimeout = function(id) {
-    delete window.timers?.[id];
-}
+// function test() {
+//     console.log("test");
+// }
 
-window.setTimeout(function(name) {
-    console.log(name);
-}, 1000, "silver surfer");
 
-window.setTimeout(function(name) {
-    console.log(name);
-}, 2000, "silver surfer 1");
-
-window.setTimeout(function(name) {
-    console.log(name);
-}, 3000, "silver surfer 2");
