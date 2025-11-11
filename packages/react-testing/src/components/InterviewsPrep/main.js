6151afa6d96ea9a9ea91ce2236db0bcf88a19c97
Abhishek Jan <abhishek.jan@brevo.com>
2025-11-11 22:02:47 +0530

    construct dom from vdom object

--- packages/react-testing/src/components/InterviewsPrep/main.js
+++ packages/react-testing/src/components/InterviewsPrep/main.js
@@ -1,10 +1,56 @@
-const btn = document.querySelector("button");
+const vdom = {
+    type: "div",
+    children: [
+        {
+            type: "h1",
+            children: "Hello"
+        },
+        {
+            type: "div",
+            children: "World"
+        },
+        {
+            type: "span",
+            children: "Good Morning"
+        },
+        "Random Text"
+    ]
+};
 
-function test() {
-   console.log("jo")
+{/* <div>
+    <h1>Hello</h1>
+    <div>World</div>
+    <span>Good Morning</span>
+    "Random Text"
+</div> */}
+
+const root = document.getElementById("root");
+
+function createDOMfromVDOM(vdom) {
+
+    if(typeof vdom === "string") {
+        return document.createTextNode(vdom);
+    }
+    const rootEl = document.createElement(vdom?.type);
+    // vdom.children as array
+    if(Array.isArray(vdom.children)) {
+        let children = [];
+        const fragment = document.createDocumentFragment();
+        vdom.children.map((child) => {
+            children.push( createDOMfromVDOM(child));
+        })
+        fragment.append(...children);
+        rootEl.appendChild(fragment);
+        return rootEl;
+    }
+    // vdom.children as a string
+    if(typeof vdom.children === "string") {
+         rootEl.textContent = vdom.children;
+         return rootEl;
+    }
 }
 
-btn.addEventListener("click", test);
+const dom = createDOMfromVDOM(vdom);
 
-btn.removeEventListener("click", test);
-
+console.log(dom);
+root.appendChild(dom);
