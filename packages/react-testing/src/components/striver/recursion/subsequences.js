8777971361a8d503a8efafa1f47dd4fb88a7a2da
Abhishek Jan <abhishek.jan@brevo.com>
2025-12-10 10:45:45 +0530

    subsequences with a fixed sum

--- packages/react-testing/src/components/striver/recursion/subsequences.js
+++ packages/react-testing/src/components/striver/recursion/subsequences.js
@@ -1,23 +1,30 @@
 const arr = [3, 1, 2];
 
-const printSubsequences = (arr) => {
+const printSubsequences = (arr, providedSum) => {
 
     let temp = [];
     let index = 0;
     const n = arr.length;
 
-    function subsequeces(index, temp) {
-        if(index >= (n)){ 
-            console.log(temp);
+    function subsequeces(index, temp, sum) {
+        if(index >= (n)){
+            if(sum === providedSum) {
+                console.log(temp);
+            }
             return;
         }
+        // Take the index
+        let newSum = sum + arr[index];
         temp.push(arr[index]);
-        subsequeces(index+1, temp);
+        subsequeces(index+1, temp, newSum);
+
+        // Not take the index
+        newSum = newSum - arr[index];
         temp.pop();
-        subsequeces(index+1, temp);
+        subsequeces(index+1, temp, newSum);
     }
 
-    subsequeces(index, temp);
+    subsequeces(index, temp, 0);
 };
 
-printSubsequences(arr);
+printSubsequences(arr, 3);
