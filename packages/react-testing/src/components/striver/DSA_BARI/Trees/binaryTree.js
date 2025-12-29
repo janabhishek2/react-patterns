ecf2ff833f7c4a7667cab51263325d4d239f5bae
Abhishek Jan <abhishek.jan@brevo.com>
2025-12-29 17:13:06 +0530

    feat: diameter of BST

--- packages/react-testing/src/components/striver/DSA_BARI/Trees/binaryTree.js
+++ packages/react-testing/src/components/striver/DSA_BARI/Trees/binaryTree.js
@@ -1,15 +1,15 @@
 const { Queue } = require("./queue");
-const { Stack } = require('../../stack-queue/stack-arr');
+const { Stack } = require("../../stack-queue/stack-arr");
 class Node {
     constructor(data) {
         this.data = data;
         this.left = null;
         this.right = null;
     }
-};
+}
 
 function max(a, b) {
-    return a > b ? a: b;
+    return a > b ? a : b;
 }
 
 class BinaryTree {
@@ -18,7 +18,7 @@ class BinaryTree {
     }
 
     preOrder(node = this.root) {
-        if(!node) return;
+        if (!node) return;
         // Root Left Right
 
         // print root
@@ -32,7 +32,7 @@ class BinaryTree {
     }
 
     inorder(node = this.root) {
-        if(!node) return;
+        if (!node) return;
         this.inorder(node.left);
 
         console.log(node.data);
@@ -41,12 +41,12 @@ class BinaryTree {
     }
 
     breadthFirstTraversal(node = this.root) {
-        if(!node) return [];
+        if (!node) return [];
         const queue = new Queue(10);
         queue.enqueue(node);
         let ans = [];
         const compute = () => {
-            if(queue.isEmpty()) return;
+            if (queue.isEmpty()) return;
             const temp = queue.dequeue();
             const currNode = temp?.data;
             ans.push(currNode);
@@ -57,7 +57,7 @@ class BinaryTree {
             left && queue.enqueue(left);
             right && queue.enqueue(right);
             !queue.isEmpty() && compute();
-        }
+        };
         compute();
         return ans;
     }
@@ -65,8 +65,8 @@ class BinaryTree {
     iterativePreOrder(node = this.root) {
         const st = new Stack(100);
         st.push(node);
-    
-        while(!st.isEmpty) {
+
+        while (!st.isEmpty) {
             const temp = st.pop();
             console.log(temp.data);
             temp?.right && st.push(temp.right);
@@ -78,7 +78,7 @@ class BinaryTree {
         const st = new Stack(100);
         st.push(node);
 
-        while(!st.isEmpty) {
+        while (!st.isEmpty) {
             let curr = st.pop();
             // push curr.right to stack
             curr?.right && st.push(curr.right);
@@ -89,10 +89,10 @@ class BinaryTree {
         }
     }
 
-    maxHeight (node = this.root) {
+    maxHeight(node = this.root) {
         function computeMax(node) {
-            let leftMax = node?.left ? computeMax(node?.left): 0;
-            let rightMax = node?.right ? computeMax(node?.right): 0;
+            let leftMax = node?.left ? computeMax(node?.left) : 0;
+            let rightMax = node?.right ? computeMax(node?.right) : 0;
 
             return 1 + max(leftMax, rightMax);
         }
@@ -100,19 +100,41 @@ class BinaryTree {
     }
 
     isBalancedBinaryTree(node = this.root) {
-        if(!node) return 0;
+        if (!node) return 0;
 
         const lh = this.isBalancedBinaryTree(node.left);
         const rh = this.isBalancedBinaryTree(node.right);
 
         // if left subtree is disbalanced, return -1 for current node;
-        if(lh === -1 || rh === -1) return -1;
+        if (lh === -1 || rh === -1) return -1;
 
         // check for balance of current node.
-        if(Math.abs(lh-rh) > 1) return -1;
+        if (Math.abs(lh - rh) > 1) return -1;
 
         // return height if left and right are balanced.
         return 1 + Math.max(lh, rh);
+    }
+
+    diamaeterOfBinaryTree(node = this.root) {
+        let maxDiameter = -1;
+        let rootElWithMaxDia = null;
+        const computeDiameter = (node) => {
+            if (!node) return 0;
+
+            const lh = computeDiameter(node?.left);
+            const rh = computeDiameter(node?.right);
+
+            const currDia = lh + rh;
+            if (currDia > maxDiameter) {
+                maxDiameter = currDia;
+                rootElWithMaxDia = node.data;
+            }
+
+            return 1 + Math.max(lh, rh);
+        };
+
+        computeDiameter(node);
+        return maxDiameter;
     }
 }
 
@@ -124,6 +146,9 @@ const node3 = new Node(3);
 const node4 = new Node(4);
 const node5 = new Node(5);
 const node6 = new Node(6);
+const node7 = new Node(7);
+const node8 = new Node(8);
+const node9 = new Node(9);
 
 root.left = node2;
 root.right = node3;
@@ -132,9 +157,13 @@ node3.left = node4;
 node3.right = node6;
 
 node4.left = node5;
+node5.left = node7;
+
+node6.right = node8;
+node8.right = node9;
 
 const bst = new BinaryTree(root);
 
-const ans = bst.isBalancedBinaryTree();
+const ans = bst.diamaeterOfBinaryTree();
 
 console.log(ans);
