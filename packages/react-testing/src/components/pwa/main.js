1c721b3b2114dae2f8c6005f97cb52bfc2147d16
Abhishek Jan <abhishek.jan@brevo.com>
2026-01-06 17:31:47 +0530

    feat: intercepting requests on sw fetch event

--- packages/react-testing/src/components/pwa/main.js
+++ packages/react-testing/src/components/pwa/main.js
@@ -3,3 +3,18 @@ if(navigator.serviceWorker) {
         console.log(registration);
     })
 }
+
+// fetch('./template.html').then((res) => {
+//     return res.text();
+// }).then((res) => {
+//     document.body.innerHTML = res;
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+fetch('./camera-feed.html').
+then((res) => res.text()).
+then((img) => {
+    const randomElement = document.querySelector("#random");
+    randomElement.innerHTML = img;
+})
--- packages/react-testing/src/components/pwa/sw.js
+++ packages/react-testing/src/components/pwa/sw.js
@@ -1,21 +1,39 @@
-console.log("SW: register")
-self.addEventListener("install", (e) => {
-    let installPromise = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(true);
-        }, 3000);
-    });
 
-    // e.waitUntil(installPromise);
-    self.skipWaiting();
-})
+// SW: register, install and activate
 
-self.addEventListener("activate", (e) => {
-    let activatePromise = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(true);
-        }, 5000);
-    })
-    e.waitUntil(activatePromise);
-    console.log("SW activate");
-})
+// console.log("SW: register")
+// self.addEventListener("install", (e) => {
+//     let installPromise = new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(true);
+//         }, 3000);
+//     });
+
+//     // e.waitUntil(installPromise);
+//     self.skipWaiting();
+// })
+
+// self.addEventListener("activate", (e) => {
+//     let activatePromise = new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(true);
+//         }, 5000);
+//     })
+//     e.waitUntil(activatePromise);
+//     console.log("SW activate");
+// })
+
+// self.addEventListener("fetch", (e) => {
+//     console.log(typeof e.request.url);
+//    if(e.request.url.toString().includes("template")) {
+//         const headers = {
+//             'Content-Type': "text/html"
+//         }
+//         const resp = new Response("<h2>I am A custom response!</h2>", {
+//             headers: headers
+//         });
+//         e.respondWith(resp);
+//    }
+// });
+
+self
