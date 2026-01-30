b5d0dcfb76103e5ecbf9fddc66ae08768da707bd
Abhishek Jan <abhishek.jan@brevo.com>
2026-01-30 11:36:58 +0530

    feat: variable buttons based on pageSize

--- packages/react-testing/src/components/ReactTutorial-Frontend-Master/Pagination/Pagination.jsx
+++ packages/react-testing/src/components/ReactTutorial-Frontend-Master/Pagination/Pagination.jsx
@@ -15,7 +15,6 @@ function Pagination({ data, renderRow, defaultPageSize = PAGE_SIZE, paginationWr
 
     const paginationClasses = `pagination ${paginationWrapperClass}`;
 
-
     const handlePrevious = () => {
         setCurrentPage((prev) => Math.max(prev - 1, 1));
     };
@@ -36,7 +35,42 @@ function Pagination({ data, renderRow, defaultPageSize = PAGE_SIZE, paginationWr
         const newPageSize = parseInt(e.target.value, 10);
         setPageSize(newPageSize);
         setCurrentPage(1);
+    };
+
+    const prevAndNextNumbers = (currIndex, lastPage) => {
+        const prev = (currIndex - 1) >= 1 ? (currIndex - 1) : null;
+        const prevPrev = (currIndex - 2) >= 1 ? (currIndex - 2) : null;
+
+        const next = (currIndex + 1) <= lastPage ? currIndex + 1: null;
+        const nextNext = (currIndex + 2) <= lastPage ? currIndex + 2: null; 
+
+        const handlePageChange = (newPage) => {
+            return () => {
+                setCurrentPage(newPage);
+            }
+        }
+        const renderNumbers = () => {
+            console.log(currIndex, prevPrev);
+            return <div style={{
+                display: "flex",
+            }}>
+                <div>...</div>
+                {prevPrev && <button disabled={currIndex === prevPrev} onClick={handlePageChange(prevPrev)}>{prevPrev}</button>}
+                {prev && <button disabled={currIndex === prev} onClick={handlePageChange(prev)}>{prev}</button>}
+
+                {<button disabled={currIndex === currentPage}>{currIndex}</button>}
+
+                {next && <button disabled={currIndex === next} onClick={handlePageChange(next)}>{next}</button>}
+                {nextNext && <button disabled={currIndex === nextNext} onClick={handlePageChange(nextNext)}>{nextNext}</button>}
+                <div>...</div>
+            </div>
+        }
+
+        return renderNumbers();
+
     }
+
+    const lastPageNumber = Math.ceil((data.length) / pageSize);
 
     return (
         <div className={paginationClasses}>
@@ -64,11 +98,17 @@ function Pagination({ data, renderRow, defaultPageSize = PAGE_SIZE, paginationWr
                 </select>
 
                 {/* First */}
-                <button onClick={handleFirst} disabled={currentPage === 1}>Go to first</button>
-                {/* prev */}
+                <button onClick={handleFirst} disabled={currentPage === 1}>First</button>
+
+                {
+                    prevAndNextNumbers(currentPage, lastPageNumber)
+                }
+
+                {/* prev
                 <button onClick={handlePrevious} disabled={currentPage === 1}>Previous Page</button>
                 {/* next */}
-                <button onClick={handleNext} disabled={currentPage === Math.ceil(data.length / pageSize)}>Next Page</button>
+                {/* <button onClick={handleNext} disabled={currentPage === Math.ceil(data.length / pageSize)}>Next Page</button> */}
+              
                 {/* Last */}
                 <button onClick={handleLast} disabled={currentPage === Math.ceil(data.length / pageSize)}>Last</button>
             </div>
