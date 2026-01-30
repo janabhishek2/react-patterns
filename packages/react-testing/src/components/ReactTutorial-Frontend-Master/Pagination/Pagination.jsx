bfff16348f41dbd6ce7841fe97e50df47e394621
Abhishek Jan <abhishek.jan@brevo.com>
2026-01-30 10:23:16 +0530

    feat: pageSize change

--- packages/react-testing/src/components/ReactTutorial-Frontend-Master/Pagination/Pagination.jsx
+++ packages/react-testing/src/components/ReactTutorial-Frontend-Master/Pagination/Pagination.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './Pagination.css';
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 5;
 
-function Pagination({ data, renderRow, pageSize = PAGE_SIZE, paginationWrapperClass }) {
+function Pagination({ data, renderRow, defaultPageSize = PAGE_SIZE, paginationWrapperClass }) {
 
     const [currentPage, setCurrentPage] = useState(1);
+    const [pageSize, setPageSize] = useState(defaultPageSize);
 
     const startindex = (currentPage - 1) * pageSize;
     const endIndex = startindex + pageSize;
@@ -23,6 +24,20 @@ function Pagination({ data, renderRow, pageSize = PAGE_SIZE, paginationWrapperCl
         setCurrentPage((prevNext) => Math.min(prevNext + 1, Math.ceil(data.length / pageSize)));
     };
 
+    const handleLast = () => {
+        setCurrentPage(Math.ceil((data.length) / pageSize));
+    }
+
+    const handleFirst = () => {
+        setCurrentPage(1);
+    };
+
+    const handlePageSizeChange = (e) => {
+        const newPageSize = parseInt(e.target.value, 10);
+        setPageSize(newPageSize);
+        setCurrentPage(1);
+    }
+
     return (
         <div className={paginationClasses}>
             <div className='pagination-content'>
@@ -39,10 +54,23 @@ function Pagination({ data, renderRow, pageSize = PAGE_SIZE, paginationWrapperCl
                 }
             </div>
             <div className='pagination-footer'>
+
+                {/* Select page size */}
+                <label htmlFor='pageSize'>Page Size: </label>
+                <select id="pageSize" onChange={handlePageSizeChange}>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                </select>
+
+                {/* First */}
+                <button onClick={handleFirst} disabled={currentPage === 1}>Go to first</button>
                 {/* prev */}
-                <button onClick={handlePrevious}>Previous</button>
+                <button onClick={handlePrevious} disabled={currentPage === 1}>Previous Page</button>
                 {/* next */}
-                <button onClick={handleNext}>Next</button>
+                <button onClick={handleNext} disabled={currentPage === Math.ceil(data.length / pageSize)}>Next Page</button>
+                {/* Last */}
+                <button onClick={handleLast} disabled={currentPage === Math.ceil(data.length / pageSize)}>Last</button>
             </div>
         </div>
     )
