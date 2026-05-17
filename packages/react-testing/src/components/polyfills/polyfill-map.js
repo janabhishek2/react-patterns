939b0927143bdc9401db791452f3d705689761fb
Abhishek Jan <abhishek.jan@brevo.com>
2026-05-17 12:45:37 +0530

    feat: map using forEach

---
+++ packages/react-testing/src/components/polyfills/polyfill-map.js
@@ -0,0 +1,23 @@
+// const arr = [1, 2, 3];
+
+// const newArr = arr.map((item) => item * 2);
+
+// console.log(newArr);
+
+const customMap = function(cb){
+    const newArr = [];
+    this.forEach((item) => {
+        const output = cb(item);
+        newArr.push(output);
+    })
+    return newArr;
+}
+
+Array.prototype.customMap = customMap;
+
+const arr = [1, 2, 3];
+const func = (item) => item * 109;
+
+const ans = arr.customMap(func);
+
+console.log(ans);
