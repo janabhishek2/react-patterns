b61c9a008dc915727835944d78842ceb3ea5e573
Abhishek Jan <abhishek.jan@brevo.com>
2026-05-19 16:00:00 +0530

    feat: depth support

--- packages/react-testing/src/components/polyfills/polyfill-array-flat.js
+++ packages/react-testing/src/components/polyfills/polyfill-array-flat.js
@@ -1,6 +1,6 @@
 const unflattened = [1, 2, [3, 4, [5, 6, [7,8]]]];
 
-function flattenArray(arr) {
+function flattenArray(arr, depth = 100) {
     const isArray = Array.isArray(arr);
     const flattened = [];
     if(isArray && arr.length == 0) return [];
@@ -8,8 +8,8 @@ function flattenArray(arr) {
     if(!isArray) return arr;
 
     arr.forEach((item) => {
-        if(Array.isArray(item)) {
-            const flattenedTemp = flattenArray(item);
+        if(Array.isArray(item) && depth > 0) {
+            const flattenedTemp = flattenArray(item, depth-1);
             flattened.push(...flattenedTemp);
         } else {
             flattened.push(item);
@@ -21,7 +21,9 @@ function flattenArray(arr) {
 
 Array.prototype.flattenArray = flattenArray;
 
-const flattened = flattenArray(unflattened);
+const flattened = flattenArray(unflattened, 1);
+
+console.log(flattened);
 
 // Trick to convert unflattened array to flattened.
 // const flat1 = unflattened.toString();
