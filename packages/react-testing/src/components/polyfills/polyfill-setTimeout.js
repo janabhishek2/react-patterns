a9ad69ae52827275dd34c9023063aedc3e9ae578
Abhishek Jan <abhishek.jan@brevo.com>
2026-05-24 13:39:25 +0530

    feat: completed timeout

---
+++ packages/react-testing/src/components/polyfills/polyfill-setTimeout.js
@@ -0,0 +1,54 @@
+const mySetTimeout = function(callbackFn, interval, ...args) {
+    let timeoutId = window.timeoutId;
+    if(!timeoutId) {
+        timeoutId = window.timeoutId = 0;
+    } else {
+        timeoutId = ++window.timeoutId;
+    }
+
+    const newTimeout = {
+        id: timeoutId,
+        exec: callbackFn,
+        args,
+        interval,
+        timeToRun: Date.now() + interval,
+    }
+
+    if(!window.timeouts) {
+        window.timeouts = [];
+    }
+
+    window.timeouts.push(newTimeout);
+    runCallbacks();
+
+    return timeoutId;
+}
+
+const clearMyTimeout = function(timeoutId) {
+    const newTimeouts = window.timeouts.filter((item) => item?.id !== timeoutId);
+
+    window.timeouts = newTimeouts;
+}
+
+const runCallbacks = function() {
+    const timeouts = window.timeouts;
+
+    timeouts.forEach((timeout) => {
+        const { exec, args, timeToRun } = timeout;
+        if(Date.now() >= timeToRun) {
+            exec(...args);
+        } else {
+            requestIdleCallback(runCallbacks)
+        }
+    })
+}
+
+const id = mySetTimeout(function(){
+    console.log("Hi");
+}, 1000);
+
+console.log(window.timeouts);
+setTimeout(() => {
+    clearMyTimeout(id);
+    console.log(window.timeouts);
+}, 5000);
