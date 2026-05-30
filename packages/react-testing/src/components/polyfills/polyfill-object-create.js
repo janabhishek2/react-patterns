f6fb7fa75305f82b9c6f2814529ae48239536481
Abhishek Jan <abhishek.jan@brevo.com>
2026-05-30 18:09:42 +0530

    feat: enumerated properties

--- packages/react-testing/src/components/polyfills/polyfill-object-create.js
+++ packages/react-testing/src/components/polyfills/polyfill-object-create.js
@@ -5,12 +5,17 @@ const animal = {
 
 // const cheetah = Object.create(animal);
 
-function myCreate(object) {
+function myCreate(object, properties) {
     const newObject = {};
+    Object.defineProperties(newObject, properties);
     Object.setPrototypeOf(newObject, object);
     return newObject;
 }
 
-const cheetah = myCreate(animal);
+const cheetah = myCreate(animal, {
+    "newValue": {
+        value: "Abcd"
+    }
+});
 
 console.log(cheetah);
