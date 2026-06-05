ae0d4cc212e0b03eb0901f3c21fb21bfc392e27b
Abhishek Jan <abhishek.jan@brevo.com>
2026-06-05 15:46:16 +0530

    dnf

---
+++ packages/react-testing/src/components/striver/recap/arrays/dnf.js
@@ -0,0 +1,55 @@
+const arr = [1, 2, 0, 0, 1, 2, 0, 0, 1];
+ 
+// 0->low-1 -> 0
+// low-mid-1 -> 1;
+// mid-high ->unsorted
+// high+1 - n-1 ->2
+
+function swap(item1, item2) {
+    let temp = item1;
+    item1 = item2;
+    item2 = temp;
+    return;
+};
+
+function sortedArray(arr) {
+
+    const n = arr.length;
+
+    let low = 0;
+    let mid = 0;
+    let high = n-1;
+
+    while(mid < high) {
+        if(arr[mid] === 0) {
+            // console.log(arr[mid], arr[low]);
+            let temp = arr[low];
+            arr[low] = arr[mid];
+            arr[mid] = temp;
+
+            // console.log(arr[mid], arr[low]);
+            low++;
+            mid++;
+        }
+
+        else if(arr[mid] === 1){
+            mid++;
+        }
+
+        else if(arr[mid] === 2) {
+            let temp = arr[mid];
+            arr[mid] = arr[high];
+            arr[high] = temp;
+            high--;
+        }
+
+        
+        else {
+            null;
+        }
+    }
+}
+
+sortedArray(arr);
+
+console.log(arr);
