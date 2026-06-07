878cd29f55f5525b88c7cc7ea25a9c605b14fb50
Abhishek Jan <abhishek.jan@brevo.com>
2026-06-08 00:34:35 +0530

    feat: reverse using l and r

--- packages/react-testing/src/components/striver/recap/rec/reverseArr.js
+++ packages/react-testing/src/components/striver/recap/rec/reverseArr.js
@@ -6,7 +6,20 @@ function reverseArr(arr) {
     return [lastEl, ...reverseArr(arr)];
 }
 
-const arr = [1, 2, 3, 4];
-const ans = reverseArr(arr);
+function reverseSecond(arr, l, r) {
+    if(l > r) return arr;
+    else {
+        // reverse items at l and r;
+        let temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+
+
+        return reverseSecond(arr, l+1, r-1);
+    }
+}
+
+const arr = [1, 2, 3, 4, 5];
+const ans = reverseSecond(arr, 0, arr.length - 1);
 
 console.log(ans);
